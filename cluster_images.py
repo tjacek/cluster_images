d640e9fe73d11ffb7c50cebeb85af48da4a0d18a
tjacek <tjacek@student.agh.edu.pl>
2015-10-27 00:17:34 +0100

    add info

--- cluster_images.py
+++ cluster_images.py
@@ -6,19 +6,23 @@ def cluster_images(in_path,out_path,n_clusters=12):
     images=create_images(in_path,n_clusters)
     inst.save_reduce("raw.csv",images)
     inst.save_clusters("cls.lb",images)
-    split_clusters(out_path,images,n_clusters)
+    split_clusters(out_path,images)
 
 def create_images(in_path,n_clusters):
     images=read_frames.read_images(in_path)
+    print("read data")
     data=inst.get_data(images)
+    print("reduce data") 
     reduced_data=reduce_dim.spectral_reduction(data)
     inst.set_reduced(images,reduced_data)
+    print("cluster data")
     img_cls=clustering.dbscan(reduced_data,n_clusters)
     inst.set_cluster(images,img_cls)
     return images
 
-def split_clusters(out_path,images,n_clusters):
+def split_clusters(out_path,images):
     utils.make_dir(out_path)
+    n_clusters=inst.number_of_clusters(images)
     cls_dirs=[out_path +"cls"+str(i)+"/" for i in range(n_clusters)]
     for c_dir in cls_dirs:
         utils.make_dir(c_dir)
