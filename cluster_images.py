7ddbe759cf4fa5fda57e4b7a87e80c6a91b3a889
tjacek <tjacek@student.agh.edu.pl>
2016-02-03 13:58:40 +0100

    new autoencoder

--- cluster_images.py
+++ cluster_images.py
@@ -1,45 +1,59 @@
-import utils,data,reduce_dim,clustering,preproc
-import scipy.misc as image
+import utils.imgs,utils.files
+import reduce_dim,clustering
+import deep.autoencoder#,reduce_dim#,#clustering,preproc
+#import scipy.misc as image
+import cv2
 
 def cluster_images(in_path,out_path,n_clusters=4):
-    images=create_images(in_path,n_clusters)
-    data.save_reduce("raw.csv",images)
-    data.save_clusters("cls.lb",images)
-    split_clusters(out_path,images)
+    dataset=create_images(in_path,n_clusters)
+    #data.save_reduce("raw.csv",images)
+    #data.save_clusters("cls.lb",images)
+    split_clusters(out_path,dataset)
 
 def create_images(in_path,n_clusters):
-    dataset=data.read_images(in_path)
+    imgs=utils.imgs.read_img_dir(in_path)
     print("read data")
-    preproc.deep_reduce(dataset)
+    #preproc.deep_reduce(dataset)
+    reduced_img=deep.autoencoder.apply_autoencoder(imgs,"../dataset/ae")
+    print(reduced_img[0].shape)
     print("preproc")
-    images=dataset.get_data()
-    print(images.shape)
-    reduced_data=reduce_dim.spectral_reduction(images)
-    dataset.set_reduced(reduced_data)
+   # images=dataset.get_data()
+   # print(images.shape)
+    mf_data=reduce_dim.spectral_reduction( reduced_img)
+   # dataset.set_reduced(reduced_data)
     print("reduce data") 
-    img_cls=clustering.dbscan(reduced_data,n_clusters)
+    img_cls=clustering.dbscan(mf_data,n_clusters)
     print("cluster data")
-    dataset.set_cluster(img_cls)
+    print(img_cls)
+    dataset=[(img_i,cls_i) for img_i,cls_i in zip(imgs,img_cls)] # dataset.set_cluster(img_cls)
     return dataset
 
 def split_clusters(out_path,dataset):
-    utils.make_dir(out_path)
-    n_clusters=dataset.get_number_of_clusters()
-    cls_dirs=[out_path +"cls"+str(i)+"/" for i in range(n_clusters)]
+    utils.files.make_dir(out_path)
+    n_clusters= 10#dataset.get_number_of_clusters()
+    cls_dirs=[out_path +"/cls"+str(i)+"/" for i in range(n_clusters)]
     for c_dir in cls_dirs:
-        utils.make_dir(c_dir)
-    for inst in dataset.instances:
-        print(inst.cls)
-        if(inst.cls>-1):
-            orginal_img=image.imread(inst.name)
-            txt_id=inst.file_id()
-            print(inst.cls)
-            img_cls=cls_dirs[inst.cls]
-            full_path=img_cls+txt_id
-            image.imsave(full_path,orginal_img)
+        utils.files.make_dir(c_dir)
+    i=0
+    for img_i,cls_i in dataset:#.instances:
+        cls_i=cls_i[1]
+        print(cls_i)
+        print(type(img_i))
+        if(cls_i>-1):
+            #orginal_img=image.imread(inst.name)
+            txt_id="/fr"+str(i)+".jpg" #inst.file_id()
+            i+=1
+            full_path=out_path
+            #print(inst.cls)
+            #img_cls=cls_dirs[inst.cls]
+            full_path=out_path+"/cls"+str(cls_i) +txt_id
+            print(full_path)
+            img_i=img_i.reshape(90,40)
+            cv2.imwrite(full_path,img_i)
+            #image.imsave(full_path,orginal_img)
 
 if __name__ == "__main__":
-    path="/home/user/cls/"
-    in_path=path+"test/"
-    out_path=path+"out2/"
+    in_path="../dataset/imgs"
+    obj_path="../dataset/ae"
+    out_path="../dataset/out"
     cluster_images(in_path,out_path)
