9e42208849c3814ce9adbc95a9b4c1719f8615a3
tjacek <tjacek@student.agh.edu.pl>
2016-02-13 23:27:10 +0100

    Agglomerative clustering

--- clustering.py
+++ clustering.py
@@ -1,8 +1,11 @@
 from sklearn import cluster
+from sklearn.cluster import AgglomerativeClustering as agglo
+from sklearn.neighbors import kneighbors_graph
 
 def kmeans(data,config):
     clusters=int(config.get('clusters',4))
-    cls=cluster.KMeans(n_clusters=clusters)
+    #cls=cluster.KMeans(n_clusters=clusters)
+    cls=cluster.MiniBatchKMeans(n_clusters=clusters)
     res=cls.fit(data)
     return cls.labels_
 
@@ -11,3 +14,10 @@ def dbscan(data,config):
     min_samples=int(config.get('min_samples',8))
     db = cluster.DBSCAN(eps, min_samples).fit(data)
     return db.labels_
+
+def agglomer(data,config):
+    connectivity = kneighbors_graph(data, n_neighbors=10, include_self=False)	
+    cls=agglo(n_clusters=6, connectivity=connectivity,
+                               linkage='ward')
+    cls.fit(data)
+    return cls.labels_
