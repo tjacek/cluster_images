9ff0f22c1197ad027504d6971e3c0d78ea486977
tjacek <tjacek@student.agh.edu.pl>
2016-03-04 16:21:49 +0100

    basic neural network implemented

--- deep/__init__.py
+++ deep/__init__.py
@@ -7,7 +7,7 @@ import utils.files as files
 import deep.tools as tools
 import autoencoder as ae
 
-def train_model(imgs,hyper_params,num_iter=500):
+def train_model_unsuper(imgs,hyper_params,num_iter=1000):
     batch_size=hyper_params["batch_size"]
     model=ae.Autoencoder(hyper_params)
     input_var=model.get_input_var()
@@ -26,3 +26,56 @@ def train_model(imgs,hyper_params,num_iter=500):
         cost_mean=np.mean(cost_e)
         print(str(epoch) + " "+str(cost_mean))
     return model
+
+def train_model_super(X,y,model,num_iter=1000):
+    batch_size=10#hyper_params["batch_size"]
+    X=X.reshape((X.shape[0],X.shape[2]))
+    input_var=model.get_input_var()
+    updates=model.get_updates()
+    train_fn = theano.function([input_var,model.target_var], model.loss, updates=updates,allow_input_downcast=True)
+    pred_fn = theano.function([input_var], model.prediction)
+    x_batch,n_batches=tools.get_batch(X,batch_size)
+    y_batch,n_batches=tools.get_batch(y,batch_size)
+    print("Number of batches " + str(len(x_batch)))
+    for epoch in range(num_iter):
+        cost_e = []
+        for i in range(n_batches):
+            x_i=x_batch[i]
+            y_i=y_batch[i]
+            #print(x_i.shape)
+            #print(y_i.shape)
+            #res=pred_fn(x_i)
+            #print(y_i)
+            loss_i= train_fn(x_i,y_i)
+            cost_e.append(loss_i)
+        cost_mean=np.mean(cost_e)
+        print(str(epoch) + " "+str(cost_mean))
+    return model
+
+#def train_model_super(X,y,model,num_iter=1000):
+#    batch_size= 10 #hyper_params["batch_size"]
+#    input_var=model.get_input_var()
+#    updates=model.get_updates()
+#    train_fn = theano.function([input_var,model.target_var], model.loss, updates=updates)
+    #train_fn = theano.function([input_var], model.prediction)#, updates=updates)
+#    input_dim=3600#(hyper_params["num_input"],)
+    #imgs=[img_i.reshape(input_dim) for img_i in imgs]
+#    batch,n_batches=tools.get_batch(imgs,batch_size)
+#    n_batches=len(y)
+#    n_cats=7
+    #print("Number of batches " + str(len(batch)))
+#    for epoch in range(num_iter):
+#        cost_e = []
+#        for i in range(n_batches):
+            #img_i=batch[i]
+#            tmp=[int(y[i])]
+#            tmp=np.array(tmp)
+            #tmp=tmp.astype(theano.config.floatX)
+            #loss_i=train_fn(X[i])
+            #print(loss_i.dtype)
+#            y_dist=tools.to_dist(y[i],n_cats)
+#            loss_i=train_fn(X[i], tmp)#y_dist)
+#            cost_e.append(loss_i)
+#        cost_mean=np.mean(cost_e)
+#        print(str(epoch) + " "+str(cost_mean))
+#    return model
--- deep/sae.py
+++ deep/sae.py
@@ -7,17 +7,31 @@ import deep.tools as tools
 
 class StackedAE(object):
     def __init__(self,autoencoder,n_cats):
+        #self.input_var = T.tensor2('inputs')
+        #self.input_var = T.tensor4('inputs')
+        self.target_var = T.ivector('targets')
     	W_hid,b_hid,W_out,b_out=autoencoder.get_numpy()
     	input_size,hidden_size=W_hid.shape
-    	self.l_in =  lasagne.layers.InputLayer(shape=(None,input_size))
+    	self.l_in =  lasagne.layers.InputLayer(shape=(None,input_size))#,input_var=self.input_var)
         self.l_hid = lasagne.layers.DenseLayer(self.l_in, num_units=hidden_size)
         softmax = lasagne.nonlinearities.softmax
         self.network = lasagne.layers.DenseLayer(self.l_hid,n_cats, nonlinearity=softmax)
-
+        self.get_loss()
+   
     def get_loss(self):
-        prediction = lasagne.layers.get_output(network)
-        loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+        self.prediction = lasagne.layers.get_output(self.network)
+        loss = lasagne.objectives.categorical_crossentropy(self.prediction, self.target_var)
         self.loss = loss.mean()
 
     def get_params(self):
-        return lasagne.layers.get_all_params(self.l_out, trainable=True)
+        return lasagne.layers.get_all_params(self.network, trainable=True)
+
+    def get_updates(self):
+        params=self.get_params()
+        return lasagne.updates.nesterov_momentum(
+                #self.prediction,
+                self.loss,
+                params, learning_rate=0.01, momentum=0.9)
+
+    def get_input_var(self):
+        return self.l_in.input_var
