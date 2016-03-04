4aa66140923de48f03cd35cb6a6d8056be84b86e
tjacek <tjacek@student.agh.edu.pl>
2016-03-04 16:41:28 +0100

    init nn

--- deep/sae.py
+++ deep/sae.py
@@ -7,12 +7,10 @@ import deep.tools as tools
 
 class StackedAE(object):
     def __init__(self,autoencoder,n_cats):
-        #self.input_var = T.tensor2('inputs')
-        #self.input_var = T.tensor4('inputs')
         self.target_var = T.ivector('targets')
     	W_hid,b_hid,W_out,b_out=autoencoder.get_numpy()
     	input_size,hidden_size=W_hid.shape
-    	self.l_in =  lasagne.layers.InputLayer(shape=(None,input_size))#,input_var=self.input_var)
+    	self.l_in =  lasagne.layers.InputLayer(shape=(None,input_size),W=W_hid,b=b_hid)#,input_var=self.input_var)
         self.l_hid = lasagne.layers.DenseLayer(self.l_in, num_units=hidden_size)
         softmax = lasagne.nonlinearities.softmax
         self.network = lasagne.layers.DenseLayer(self.l_hid,n_cats, nonlinearity=softmax)
