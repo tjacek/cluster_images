708d7f697d1e97282342c54e373d54847f8d29d8
tjacek <tjacek@student.agh.edu.pl>
2016-03-07 19:52:28 +0100

    center of mass

--- shape_context/__init__.py
+++ shape_context/__init__.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_shape_context(in_path):
     points=read_points(in_path)
-    print(len(points))
+    center=center_of_mass(points)
 
 def read_points(in_path):
     img=cv2.imread(in_path,cv2.IMREAD_GRAYSCALE)
@@ -13,8 +13,15 @@ def read_points(in_path):
           points.append(make_point(x,y))
     return points
 
+def center_of_mass(points):
+    np_points=np.array(points)
+    size=float(len(points))
+    center=np_points.sum(axis=0)
+    center/=size
+    return center
+
 def make_point(x,y):
-    return np.array([x,y]) 
+    return np.array([x,y],dtype=float) 
 
 if __name__ == "__main__":
     get_shape_context("in.jpg")
