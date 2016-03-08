60f488000aa4cde5d17c6c6466a45b2142df6d45
tjacek <tjacek@student.agh.edu.pl>
2016-03-08 20:19:19 +0100

    simple classification

--- seq/dtw.py
+++ seq/dtw.py
@@ -1,13 +1,28 @@
 import utils.files as files
 import numpy as np 
 import seq
+from collections import Counter
+from sklearn.metrics import classification_report
 
 def wrap(in_path): 
     str_seqs=files.read_file(in_path)
     instances=seq.get_seqs(str_seqs)
-    print(dwt_metric(instances[-1],instances[77]))
-    print(instances)
-    #k=action_to_vec_seq(str_seq,10)
+    test,train=select_dataset(instances)
+    correct=[test_i.cat for test_i in test]
+    pred=[knn(test_i,train) for test_i in test]
+    print(classification_report(correct, pred))
+
+
+def knn(inst,instances,k=6):
+    dists=[dwt_metric(inst,inst_i) for inst_i in instances]
+    dists=np.array(dists)
+    dist_inds=dists.argsort()[0:k]
+	#nearest=[dists[i] for i in dist_inds]
+    nearest=[instances[i].cat for i in dist_inds]
+    print(nearest)
+    count =Counter(nearest)
+    new_cat=count.most_common()[0][0]
+    return new_cat
 
 def dwt_metric(s,t):
     n=len(s)
@@ -22,8 +37,18 @@ def dwt_metric(s,t):
         for j in range(1,m+1):
             cost=d(s[i-1],t[j-1])
             dwt[i,j]=cost+min([dwt[i-1][j],dwt[i][j-1],dwt[i-1][j-1]])
-    print(dwt)
     return dwt[n][m]
 
 def d(v,d):
     return np.linalg.norm(v-d)
+
+
+def select_dataset(instances):
+    train=[]
+    test=[]	
+    for i,inst_i in enumerate(instances):
+        if((i % 2) ==0):
+            train.append(inst_i)
+        else:
+            test.append(inst_i)	
+    return train,test
