086cc100ff557253c11d5fb663dadf49a04b6e9c
tjacek <tjacek@student.agh.edu.pl>
2016-03-09 22:08:45 +0100

    dict added

--- clustering.py
+++ clustering.py
@@ -2,6 +2,8 @@ from sklearn import cluster
 from sklearn.cluster import AgglomerativeClustering as agglo
 from sklearn.neighbors import kneighbors_graph
 
+ALGS={'kmeans':kmeans,'dbscan':dbscan,'agglomer':agglomer}
+
 def kmeans(data,config):
     clusters=int(config.get('clusters',4))
     #cls=cluster.KMeans(n_clusters=clusters)
@@ -16,8 +18,9 @@ def dbscan(data,config):
     return db.labels_
 
 def agglomer(data,config):
+    clusters=int(config.get('clusters',6))
     connectivity = kneighbors_graph(data, n_neighbors=10, include_self=False)	
-    cls=agglo(n_clusters=6, connectivity=connectivity,
+    cls=agglo(n_clusters=clusters, connectivity=connectivity,
                                linkage='ward')
     cls.fit(data)
     return cls.labels_
