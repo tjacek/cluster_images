9e258f0fd06de47bdf7996106d0782dc0f5ad31d
tjacek <tjacek@student.agh.edu.pl>
2016-03-13 22:35:10 +0100

    extract features

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,5 +1,14 @@
 import numpy as np
+import utils.pcloud as pcloud
 from sklearn.decomposition import PCA
+
+def get_features(img):
+    pcloud=make_point_cloud(img)
+    extractors=[]
+    all_feats=[]
+    for extr_i in extractors:
+        all_feats+=extr_i(pcloud)    	
+    return all_feats
 
 def pca_features(pcloud):
 	feats=[]
