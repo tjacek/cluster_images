a256a77147d938b9c068eb02cdbda7e8183bdb10
tjacek <tjacek@student.agh.edu.pl>
2016-03-14 18:16:39 +0100

    correlation features

--- basic/__init__.py
+++ basic/__init__.py
@@ -1,13 +1,19 @@
 import numpy as np
 import utils.pcloud as pcloud
+import scipy.stats
 from sklearn.decomposition import PCA
 
 def get_features(img):
     points=pcloud.make_point_cloud(img)
-    extractors=[pca_features]
+    if(points==None):
+    	return None
+    cloud_extractors=[corl_features]
     all_feats=[]
-    for extr_i in extractors:
+    for extr_i in cloud_extractors:
         all_feats+=extr_i(points)
+    img_extractors=[height_feat]
+    for extr_i in img_extractors:
+        all_feats+=extr_i(img)
     print(all_feats)      	
     return np.array(all_feats)
 
@@ -19,3 +25,20 @@ def pca_features(pcloud):
     for comp_i in pca.components_:
         feats+=list(comp_i)	
     return feats
+
+def corl_features(pcloud):
+    feats=[]
+    points=pcloud.get_numpy()
+    dim=pcloud.dims
+    print(dim)
+    for x_i in range(dim):
+        for y_i in range(dim):
+            if(x_i!=y_i):
+                corr_xy=scipy.stats.pearsonr(points[x_i,:],points[y_i,:])
+                feats.append(corr_xy[0])    
+    print(feats)
+    return feats
+
+def height_feat(img):
+    x,y=img.shape
+    return [float(x)/float(y)]
