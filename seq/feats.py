80f93d99e7e0af6233cb54566c9240c969cd6748
tjacek <tjacek@student.agh.edu.pl>
2016-03-16 07:59:48 +0100

    autoencoder features

--- seq/feats.py
+++ seq/feats.py
@@ -1,14 +1,16 @@
 import utils
+import utils.files as files
 import basic
 import seq
 
 def to_vec_seq(action_path):
     actions=utils.apply_to_dir(action_path)
-    vec_seqs=[ action_to_vecs(action_i) for action_i in actions]
+    extr=auto_extractor()
+    vec_seqs=[ action_to_vecs(action_i,extr) for action_i in actions]
     return vec_seqs
 
-def action_to_vecs(action):
-    vecs=[basic_feat(frame_i) for frame_i in action.frames]
+def action_to_vecs(action,feat_extractor):
+    vecs=[feat_extractor(frame_i) for frame_i in action.frames]
     return seq.Seq(vecs,action.cat)
 
 def basic_feat(img):
@@ -17,7 +19,12 @@ def basic_feat(img):
 
 def int_cats(action_i):
     cat_to_int={}	
-	for action_i in actions:
+    for action_i in actions:
         cat_id=cat_to_int.get(action_i.cat,None)
         if(cat_id==None):
-            cat_to_int[cat_id]=len(cat_to_int.keys())	 
+            cat_to_int[cat_id]=len(cat_to_int.keys())
+
+def auto_extractor():
+    ae_path="../dataset6/ae"
+    ae=files.read_object(ae_path)
+    return lambda img:ae.apply(img)  
