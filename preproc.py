4b936b4c1bd341a98b77a451c122a02d0877d4e2
tjacek <tjacek@student.agh.edu.pl>
2016-04-29 22:33:41 +0200

    ok

--- preproc.py
+++ preproc.py
@@ -1,8 +1,10 @@
 import deep
-import utils.imgs ,utils.dirs#as image
+import utils.imgs ,utils.dirs,utils.files
 import deep.autoencoder as ae
 import utils.conf
 from utils.dirs import dir_arg, ApplyToFiles
+import cv2
+import numpy as np
 
 def create_autoencoder(conf_dict):
     image_path=conf_dict["img_path"]
@@ -10,14 +12,14 @@ def create_autoencoder(conf_dict):
     print("read images from "+ image_path)
     dim_x=int(conf_dict['dim_x'])
     dim_y=int(conf_dict['dim_y'])
-    utils.dirs.make_dir(obj_path)
-    @ApplyToFiles(True)
+    #utils.dirs.make_dir(obj_path)
+    #@ApplyToFiles(True)
     def inner_func(in_path,out_path):
         print(str(in_path))
         print(str(out_path))
         imgs=utils.imgs.read_images(str(in_path))
         print("Number of images %i",len(imgs))
-        da=deep.train_model_unsuper(imgs,ae.default_parametrs(),input_dim=(dim_x,dim_y))
+        da=deep.train_model_unsuper(imgs,ae.default_parametrs(),num_iter=500,input_dim=(dim_x,dim_y))
         utils.files.save_object(da,str(out_path)) 
         print("autoencoder saved as " + str(out_path))
     inner_func(image_path,obj_path)
@@ -31,16 +33,22 @@ def read_autoencoder(obj_path):
     model=utils.read_object(obj_path)
     return ae.AutoEncoder(model)
 
-def apply_ae(image_path,ae_path):
-    imgs=utils.imgs.read_img_dir(image_path)
-    #ae.read_autoencoder(ae_path)
-    reduced=ae.apply_autoencoder(imgs,ae_path)
-    lines=[utils.files.vector_string(img_i) for img_i in reduced]
-    lines=utils.files.array_to_txt(lines,"\n")
-    utils.files.save_string("auto.csv",lines)
+@ApplyToFiles(dir_arg=True)
+def apply_ae(in_path,out_path,ae_path):
+    ae_model=utils.files.read_object(ae_path)
+    imgs=utils.imgs.read_images(in_path)
+    transform_img=lambda img_i:ae_model.apply(img_i)
+    transformed_imgs=[transform_img(img_i) for img_i in imgs]
+    utils.files.make_dir(out_path)
+    for i,trans_img_i in enumerate(transformed_imgs):
+        full_path=out_path+"/"+"img_" + str(i) +".jpg"
+        trans_img_i=np.reshape(trans_img_i,(120,60))
+        trans_img_i*=250.0
+        cv2.imwrite(full_path,trans_img_i)
+   
 
 if __name__ == "__main__":
-    conf_path="conf/dataset9.cfg"
+    conf_path="conf/dataset1.cfg"
     conf_dict=utils.conf.read_config(conf_path)
-    create_autoencoder(conf_dict)
-    #apply_ae(img_path,ae_path)
+    #create_autoencoder(conf_dict)
+    apply_ae(conf_dict["action_path"],"recon1",conf_dict["ae_path"])
