7d7fbb4c30c7722b28ed915cbdcf7b19fcf69026
tjacek <tjacek@student.agh.edu.pl>
2016-05-02 11:32:20 +0200

    test connet

--- deep/__init__.py
+++ deep/__init__.py
@@ -7,16 +7,22 @@ import utils.files as files
 import deep.tools as tools
 import autoencoder as ae
 
-def train_model_unsuper(imgs,hyper_params,num_iter=1000):
+def train_model_unsuper(imgs,hyper_params,num_iter=500,input_dim=(60,60)):
     batch_size=hyper_params["batch_size"]
+    input_dim=(np.product(imgs[0].shape),) 
+    hyper_params["num_input"]=np.product(input_dim)
     model=ae.Autoencoder(hyper_params)
     input_var=model.get_input_var()
     updates=model.get_updates()
     train_fn = theano.function([input_var], model.loss, updates=updates)
-    input_dim=(hyper_params["num_input"],)
+    #(hyper_params["num_input"],)
+    print(input_dim)
+    print("Input img size:" + str(imgs[0].shape))
+    #imgs=[img_i.reshape(input_dim) for img_i in imgs]
     imgs=[img_i.reshape(input_dim) for img_i in imgs]
     batch,n_batches=tools.get_batch(imgs,batch_size)
     print("Number of batches " + str(len(batch)))
+    print("Number of iters " + str(num_iter))
     for epoch in range(num_iter):
         cost_e = []
         for i in range(n_batches):
@@ -27,15 +33,15 @@ def train_model_unsuper(imgs,hyper_params,num_iter=1000):
         print(str(epoch) + " "+str(cost_mean))
     return model
 
-def train_model_super(X,y,model,batch_size=100, num_iter=1000):
+def train_model_super(X,y,model,batch_size=100, num_iter=250):
     X=X.reshape((X.shape[0],X.shape[1]*X.shape[2]))
     input_var=model.get_input_var()
     updates=model.get_updates()
     train_fn = theano.function([input_var,model.target_var], model.loss, updates=updates,allow_input_downcast=True)
-    #pred_fn = theano.function([input_var], model.prediction)
     x_batch,n_batches=tools.get_batch(X,batch_size)
     y_batch,n_batches=tools.get_batch(y,batch_size)
     print("Number of batches " + str(len(x_batch)))
+    print("Number of iters " + str(num_iter))
     for epoch in range(num_iter):
         cost_e = []
         for i in range(n_batches):
@@ -46,3 +52,20 @@ def train_model_super(X,y,model,batch_size=100, num_iter=1000):
         cost_mean=np.mean(cost_e)
         print(str(epoch) + " "+str(cost_mean))
     return model
+
+def test_model(X,model,batch_size=100, num_iter=250):
+    print(X.shape)
+    X=to_conv(X,dim=60)
+    print(X.shape)
+    x_batch,n_batches=tools.get_batch(X,batch_size)
+    for epoch in range(num_iter):
+        for i in range(n_batches):
+            x_i=x_batch[i]
+            print(x_i.dtype)
+            print(model.loss(x_i))
+
+def to_conv(X,dim=60):
+    n_img=X.shape[0]
+    X_conv=[np.reshape(X[i],(1,dim,dim)) for i in range(n_img)]
+    X_conv=np.array(X_conv)
+    return X_conv
--- deep/convnet.py
+++ deep/convnet.py
@@ -3,23 +3,36 @@ import theano
 import theano.tensor as T
 import lasagne
 
+class Convet(object):
+    def __init__(self,l_in,l_out,loss,updates):
+        self.l_in=l_in
+        self.l_out=l_out
+        #self.target_var=
+        self.loss=loss
+        self.updates=updates
+
+    #def get_input_var(self):
+    #    return self.l_in.input_var
+
+    def get_updates(self):
+        return self.updates
 
 def build_convnet(params,n_cats):
-	input_shape=(None, 1, params["dimX"], params["dimY"]),
-	n_filters=params["num_filters"]
-	fliter_size2D=(params["filter_size"],params["filter_size"])
-	pool_size2D=(params["pool_size"],params["pool_size"])
+    input_shape=(None, 1, params["dimX"], params["dimY"])
+    n_filters=params["num_filters"]
+    filter_size2D=(params["filter_size"],params["filter_size"])
+    pool_size2D=(params["pool_size"],params["pool_size"])
     p_drop=params["p"]
-	in_layer = lasagne.layers.InputLayer(
-		       shape=input_shape
-               input_var=input_var)
-	conv_layer1 = lasagne.layers.Conv2DLayer(
+    in_layer = lasagne.layers.InputLayer(
+		       shape=input_shape)
+               #input_var=input_var)
+    conv_layer1 = lasagne.layers.Conv2DLayer(
             in_layer, num_filters=n_filters, filter_size=filter_size2D,
             nonlinearity=lasagne.nonlinearities.rectify,
             W=lasagne.init.GlorotUniform())
-	pool_layer1 = lasagne.layers.MaxPool2DLayer(conv_layer1, pool_size=pool_size2D)
+    pool_layer1 = lasagne.layers.MaxPool2DLayer(conv_layer1, pool_size=pool_size2D)
     conv_layer2 = lasagne.layers.Conv2DLayer(
-            pool_layer1, num_filters=num_filters, filter_size=filter_size2D,
+            pool_layer1, num_filters=n_filters, filter_size=filter_size2D,
             nonlinearity=lasagne.nonlinearities.rectify)
     pool_layer2 = lasagne.layers.MaxPool2DLayer(conv_layer2, pool_size=pool_size2D)
     dropout = lasagne.layers.DenseLayer(
@@ -30,6 +43,26 @@ def build_convnet(params,n_cats):
             lasagne.layers.dropout(dropout, p=p_drop),
             num_units=n_cats,
             nonlinearity=lasagne.nonlinearities.softmax)
+    all_layers=[in_layer,conv_layer1,pool_layer1,
+                conv_layer2,pool_layer2,dropout,out_layer ]
+    loss,pred=get_loss(in_layer,out_layer)
+    updates=None#get_updates(loss,out_layer)
+    return Convet(in_layer,out_layer,loss,updates)
+
+def get_loss(in_layer,out_layer):
+    in_var=in_layer.input_var
+    prediction = lasagne.layers.get_output(out_layer)
+    print(in_var.type)
+    #loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+    #loss = loss.mean()
+    loss=theano.function([in_var], prediction)
+    return loss,prediction
+
+def get_updates(loss,out_layer):
+    params = lasagne.layers.get_all_params(out_layer, trainable=True)
+    updates = lasagne.updates.nesterov_momentum(
+            loss, params, learning_rate=0.01, momentum=0.9)
+    return updates
 
 def default_params():
     return {"dimX":60,"dimY":60,"num_filters":32,
