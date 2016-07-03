71d4ab6d6b85ee636fc308ee4eb7190f91746d15
tjacek <tjacek@student.agh.edu.pl>
2016-07-03 17:10:52 +0200

    compile lstm

--- deep/lstm.py
+++ deep/lstm.py
@@ -3,12 +3,20 @@ import numpy as np
 import theano
 import theano.tensor as T
 
+class LstmModel(object):
+    def __init__(self,input_vars,loss,updates):
+        self.input_vars=input_vars
+        vars_list=[input_vars['in_var'],input_vars['target_var'],input_vars['mask_var']]
+        self.train = theano.function(vars_list,loss, updates=updates)
+        self.loss = theano.function(vars_list, loss)
+
 def make_LSTM(hyper_params):
     n_batch=hyper_params['n_batch']
     max_seq=hyper_params['max_seq']
     seq_dim=hyper_params['seq_dim']
-    n_hidden=10
-    grad_clip = 100
+    n_cats=hyper_params['n_cats']
+    n_hidden=hyper_params['n_hidden']
+    grad_clip = hyper_params['grad_clip']
     l_in = lasagne.layers.InputLayer(shape=(n_batch, max_seq, seq_dim))
     l_mask = lasagne.layers.InputLayer(shape=(n_batch, max_seq))
     l_forward = lasagne.layers.RecurrentLayer(
@@ -25,8 +33,27 @@ def make_LSTM(hyper_params):
     l_backward_slice = lasagne.layers.SliceLayer(l_backward, 0, 1)
     l_sum = lasagne.layers.ConcatLayer([l_forward_slice, l_backward_slice])
     l_out = lasagne.layers.DenseLayer(
-        l_sum, num_units=1, nonlinearity=lasagne.nonlinearities.tanh)
-    return l_out
+        l_sum, num_units=n_cats, nonlinearity=lasagne.nonlinearities.softmax)
+    input_vars=make_input_vars(l_in,l_mask)
+    return l_out,input_vars
 
-#def get_hyper_params(masked_dataset):
-	
+def compile_lstm(lstm_equ,input_vars,hyper_params):
+    prediction = lasagne.layers.get_output(lstm_equ)
+    loss = lasagne.objectives.categorical_crossentropy(prediction,input_vars['target_var'])
+    loss = loss.mean()
+    params = lasagne.layers.get_all_params(lstm_equ, trainable=True)
+    updates = lasagne.updates.adagrad(loss,params, hyper_params['learning_rate'])
+    return LstmModel(input_vars,loss,updates)
+    
+def make_input_vars(l_in,l_mask):
+    in_var=l_in.input_var
+    target_var = T.ivector('targets')
+    mask_var=l_mask.input_var
+    return {'in_var':in_var,'target_var':target_var,'mask_var':mask_var}
+
+def get_hyper_params(masked_dataset):
+    hyper_params=masked_dataset['params']
+    hyper_params['n_hidden']=10
+    hyper_params['grad_clip']=100
+    hyper_params['learning_rate']=0.001
+    return hyper_params	
--- seq/__init__.py
+++ seq/__init__.py
@@ -4,9 +4,12 @@ import utils
 import utils.files as files
 import numpy as np
 import to_dataset
+import deep.lstm
 
 if __name__ == "__main__":
     path='../dataset0/seq/'
     dataset=to_dataset.seq_dataset(path)
     new_dataset=to_dataset.masked_dataset(dataset)
-    print(new_dataset['params'])
+    hyper_params=deep.lstm.get_hyper_params(new_dataset)
+    lstm_equ,input_vars=deep.lstm.make_LSTM(hyper_params)
+    deep.lstm.compile_lstm(lstm_equ,input_vars,hyper_params)
