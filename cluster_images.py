44da1164c314f23b4930df0bcfb05567932ce9e1
tjacek <tjacek@student.agh.edu.pl>
2016-09-05 13:00:56 +0200

    load img

--- cluster_images.py
+++ cluster_images.py
@@ -1,5 +1,5 @@
 import utils.imgs,utils.files,utils.conf,utils.actions
-import select_imgs
+#import select_imgs
 import select_imgs.clustering
 import select_imgs.tools
 import numpy as np
@@ -8,26 +8,29 @@ import utils.dirs
 #from basic.external import external_features
 from preproc import select_extractor
 
-def cluster_images(in_path,out_path):
-    data=imgs.make_imgs(in_path,norm=True)
-    extractor=select_extractor(conf_dict)
-    cls_alg=select_imgs.clustering.DbscanAlg()
-    cls_alg(data)
+def cluster_images(conf_path):
+    in_path=conf_path['img_path']
+    out_path=conf_path['cls_path']
+    data=utils.imgs.make_imgs(in_path,norm=True)
+    print(type(data[0]))
+    #extractor=select_extractor(conf_dict)
+    #cls_alg=select_imgs.clustering.DbscanAlg()
+    #cls_alg(data)
     
-def cluster_images(in_path,out_path):
-    @ApplyToFiles(True)
-    def inner_func(in_cat,out_cat):
-        print(str(in_cat)+"\n"+str(out_path)+"\n")
-        new_config=config.copy()
-        new_config["in_path"]=str(in_cat)
-        new_config["out_path"]=str(out_cat)
-        if(config['init_features']=='autoencoder'):
-            new_config["ae_path"]=config["ae_path"]+"/"+in_cat.get_name()
-        imgs=utils.imgs.read_images(in_cat)
-        print("Number of images %i",len(imgs))
-        dataset,n_clusters=select_imgs.create_images(in_cat,new_config,imgs)
-        select_imgs.tools.split_clusters(new_config,dataset,n_clusters)
-    inner_func(in_path,out_path)
+#def cluster_images(in_path,out_path):
+#    @ApplyToFiles(True)
+#    def inner_func(in_cat,out_cat):
+#        print(str(in_cat)+"\n"+str(out_path)+"\n")
+#        new_config=config.copy()
+#        new_config["in_path"]=str(in_cat)
+#        new_config["out_path"]=str(out_cat)
+#        if(config['init_features']=='autoencoder'):
+#            new_config["ae_path"]=config["ae_path"]+"/"+in_cat.get_name()
+#        imgs=utils.imgs.read_images(in_cat)
+#        print("Number of images %i",len(imgs))
+#        dataset,n_clusters=select_imgs.create_images(in_cat,new_config,imgs)
+#        select_imgs.tools.split_clusters(new_config,dataset,n_clusters)
+#    inner_func(in_path,out_path)
 
 def create_cat_images(in_path,config): 
     init_imgs=use_init_features(in_path,config)
@@ -39,6 +42,6 @@ def create_cat_images(in_path,config):
     return dataset,n_clusters
 
 if __name__ == "__main__":
-    conf_path="conf/dataset9.cfg"
-    #cluster_images(conf_path)
-    cluster_images(conf_path)
+    conf_path="conf/dataset3.cfg"
+    conf_dict=utils.conf.read_config(conf_path)
+    cluster_images(conf_dict)
