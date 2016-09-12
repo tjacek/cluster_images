dc6fd79b79f4599019fc94f11966e4e11d366303
tjacek <tjacek@student.agh.edu.pl>
2016-09-12 12:25:35 +0200

    save kernels

--- deep/kernels.py
+++ deep/kernels.py
@@ -1,24 +1,43 @@
 import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
+import numpy as np
+import cv2
 import deep.reader
+import utils.dirs
 
 def get_kernels(conv_net):
     model=conv_net.get_model()
     params=model.params
     filter_size=model.hyperparams["filter_size"]
     kernels=select_kernels(params,filter_size)
-    print(len(kernels))
-    #inspect(params[0])
-    #print(params)
+    kernels=flatten_kernels(kernels[0])
+    show_shapes(kernels)
+    save_kernels(kernels,'kernels')
 
 def select_kernels(params,filter_size):
 	return [param_i 
 	          for param_i in params
 	            if param_i.shape[-2:]==filter_size]
 
+def flatten_kernels(kernel_numpy):
+    return [np.reshape(kernel_i, (-1,5)) 
+            for kernel_i in kernel_numpy]
+
 def inspect(model):
     print(type(model))
     print(dir(model))
+
+def show_shapes(arrays):
+    shapes=[array_i.shape for array_i in arrays]
+    print(shapes)
+
+def save_kernels(kernels,out_path):
+    utils.dirs.make_dir(out_path)
+    paths=[out_path +'/cls'+str(i)+'.jpg'
+            for i,kern_i in enumerate(kernels)]
+    for kern_i,path_i in zip(kernels,paths):
+        kern_i*=200
+        cv2.imwrite(path_i,kern_i)
 
 if __name__ == "__main__":
     nn_path="../dataset2/conv_nn"
