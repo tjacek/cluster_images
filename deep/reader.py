b401ceaddc58e48a974f8b8c2d7df80b3ec8bbcf
tjacek <tjacek@student.agh.edu.pl>
2016-09-20 07:37:22 +0200

    preprocin reader

--- deep/reader.py
+++ deep/reader.py
@@ -12,17 +12,18 @@ class NNReader(object):
                       'Autoencoder':compile_autoencoder,
                       'ConvAutoencoder':compile_conv_ae}
 
-    def read(self,in_path,determistic=True):
+    def read(self,in_path,preproc, drop_p=0.0):#determistic=False):
         model=self.__unpickle__(in_path)
-        if(determistic):
-            model.set_determistic()
+        #if(determistic):
+        #    model.set_determistic()
+        model.hyperparams['p']=drop_p
         print(model.type_name)
         type_reader=self.types[model.type_name]
-        neural_net=type_reader(model.hyperparams)
+        neural_net=type_reader(model.hyperparams,preproc)
         neural_net.set_model(model)
         return neural_net
     
     def __unpickle__(self,in_path):
-        with open(in_path, 'r') as f:
+        with open(str(in_path), 'r') as f:
             model = pickle.load(f)
         return model	
