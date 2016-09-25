118b148ad16b82393b9caf7316d4714693848485
tjacek <tjacek@student.agh.edu.pl>
2016-09-25 08:39:10 +0200

    combine extractor type

--- preproc.py
+++ preproc.py
@@ -3,11 +3,13 @@ import utils.imgs as imgs
 import utils.dirs,utils.files
 import utils.data as data
 import deep.reader 
+import deep.tools 
 import utils.conf
 import numpy as np
 import basic
 import basic.reduction
 import basic.external
+import basic.combine
 from basic.external import external_features
 
 def transform_features(conf_dict):
@@ -26,22 +28,26 @@ def make_features(conf_dict):
 
 def select_extractor(conf_dict):
     extractor_type=conf_dict['extractor']
+    preproc3D=deep.tools.ImgPreproc()
     if(extractor_type=='deep'):
         nn_path=conf_dict['nn_path']
         nn_reader=deep.reader.NNReader()
-        extractor=nn_reader.read(nn_path)
+        extractor=nn_reader.read(nn_path,preproc3D)
     elif extractor_type=='text':
         text_path=conf_dict['text_path']
         feat_dict=basic.external.read_external(text_path)
         extractor=lambda img_i:feat_dict[img_i.name]
     elif extractor_type=='basic':
         extractor=basic.get_features
+    elif extractor_type=='combine':
+        combine_path=conf_dict['combine_path']
+        extractor=basic.combine.build_combined(combine_path)
     else:
         extractor=getattr(basic.reduction,extractor_type)
     return extractor
 
 if __name__ == "__main__":
-    conf_path="conf/dataset2.cfg"
+    conf_path="conf/dane.cfg"
     conf_dict=utils.conf.read_config(conf_path)
     make_features(conf_dict)
     #transform_features(conf_dict)
