eca536f58deffc0953a410338f00a09eff0dc3f4
tjacek <tjacek@student.agh.edu.pl>
2016-09-25 08:36:30 +0200

    build combine extractor

--- basic/combine.py
+++ basic/combine.py
@@ -12,12 +12,14 @@ class CombinedFeatures(object):
     def __call__(self,img_i):
         feats=[extr_i(img_i) 
     	        for extr_i in self.extractors]
+    	feats=np.concatenate(feats)
+    	print(feats.shape)
     	return feats
 
 def build_combined(in_path):
     all_paths=utils.dirs.all_files(in_path)
     nn_reader=deep.reader.NNReader()
-    extractors=[nn_reader.read(nn_path_i,0.3)
+    extractors=[nn_reader.read(nn_path_i,0.0)
                   for nn_path_i in all_paths]
     return CombinedFeatures(extractors)
 
@@ -25,5 +27,5 @@ if __name__ == "__main__":
     in_path='../dane/nn'
     img_path='../dane/train_last'
     extractor=build_combined(in_path)
-    imgs=utils.imgs.read_img(img_path)
-    print(extractor(img[0]))
+    imgset=utils.imgs.make_imgs(img_path,norm=True)
+    extractor(imgset[0])
