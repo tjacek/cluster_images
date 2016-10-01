30ea9432ec3f13c94568bae7d9444d9749ba8b04
tjacek <tjacek@student.agh.edu.pl>
2016-10-01 22:58:51 +0200

    remove unused function

--- deep/autoconv.py
+++ deep/autoconv.py
@@ -24,21 +24,21 @@ class ConvAutoencoder(deep.NeuralNetwork):
                                updates=updates,allow_input_downcast=True)
 
     def reconstructed(self,in_img):
-        img4D=self.preproc(in_img)
+        img4D=self.preproc.apply(in_img)
         raw_rec=self.__reconstructed__(img4D)
         img2D=tools.postproc3D(raw_rec)
         print(img2D.shape)
         return imgs.Image(in_img.name,img2D,in_img.org_dim)
 
     def __call__(self,in_img):
-        img4D=self.preproc(in_img)
+        img4D=self.preproc.apply(in_img)
         return self.__prediction__(img4D).flatten()
 
 def default_parametrs():
-    return {"num_input":(None,2,60,60),"num_hidden":600,"batch_size":100,
+    return {"num_input":(None,3,60,60),"num_hidden":300,"batch_size":100,
             "num_filters":16,"filter_size":(5,5),"pool_size":(4,4)}  
 
-def compile_conv_ae(hyper_params):
+def compile_conv_ae(hyper_params,preproc):
     l_hid,l_out,in_var=build_conv_ae(hyper_params)
     params = lasagne.layers.get_all_params(l_out, trainable=True)
     target_var = T.ivector('targets')
@@ -48,12 +48,14 @@ def compile_conv_ae(hyper_params):
     l1_penalty = regularize_layer_params(l_hid, l1) * 0.0001
     loss+=l1_penalty  
     updates=lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.001, momentum=0.8) 
-    return ConvAutoencoder(hyper_params,l_out,tools.preproc3D,in_var,
+    return ConvAutoencoder(hyper_params,l_out,preproc,in_var,
                          reduction,reconstruction,loss,updates)    
 
 def build_conv_ae(hyper_params):
     num_hidden=hyper_params["num_hidden"]
     input_shape = hyper_params["num_input"]
+    #print(input_shape)
+    #print("@@@@@@@@@@@@@@@@@@@@@")
     n_filters = hyper_params["num_filters"]
     filter_size2D = hyper_params["filter_size"]
     pool_size2D =  hyper_params["pool_size"]
@@ -85,30 +87,23 @@ def build_conv_ae(hyper_params):
     conv_layer2=lasagne.layers.Upscale2DLayer(pool_layer2, pool_size2D)
     
     out_layer= lasagne.layers.TransposedConv2DLayer(
-            conv_layer2, num_filters=2, filter_size=filter_size2D,
+            conv_layer2, num_filters=3, filter_size=filter_size2D,
             nonlinearity=None,#lasagne.nonlinearities.rectify,
             W=lasagne.init.GlorotUniform())
     
     in_var=l_in.input_var
     print(lasagne.layers.get_output_shape( out_layer))
     return hidden,out_layer,in_var#l_hid,l_out,in_var
-   
-def read_conv_ae(path):
-    with open(path, 'r') as f:
-        model = pickle.load(f)
-    #nn.layers.set_all_param_values(model, data)
-    conv_net=compile_autoencoder(model.hyperparams)
-    conv_net.set_model(model)
-    return conv_net
 
 if __name__ == "__main__": 
-    path_dir="../dataset1/cats"
-    ae_path="../dataset1/conv_ae_"
-    imgset=imgs.make_imgs(path_dir,norm=True,transform=imgs.to_3D)
+    path_dir="../dane2/proj"
+    ae_path="../dane2/conv_ae"
+    preproc=tools.ImgPreproc()
+    imgset=imgs.make_imgs(path_dir,norm=True,transform=preproc)
     print(imgset.shape)
-    #model= read_conv_ae(ae_path)
     nn_reader=deep.reader.NNReader()
-    model= nn_reader.read(ae_path)
-    #model=compile_conv_ae(default_parametrs())
-    model=deep.train.test_unsuper_model(imgset,model,num_iter=500)
+    model= nn_reader.read(ae_path,nn_reader)
+    
+    #model=compile_conv_ae(default_parametrs(),preproc)
+    model=deep.train.test_unsuper_model(imgset,model,num_iter=100)
     model.get_model().save(ae_path)
