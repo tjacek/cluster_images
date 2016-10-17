64f341e95851472ed81a383666c4d14b72fb4c59
tjacek <tjacek@student.agh.edu.pl>
2016-10-17 23:46:45 +0200

    ok

--- cats.py
+++ cats.py
@@ -3,14 +3,17 @@ import seq.features
 import utils.conf
 import utils.actions
 
-def make_seq_files(conf_path, new_features=True):
+def make_seq_files(conf_path, new_features=True,short_names=True):
     conf_dict=utils.conf.read_config(conf_path)
     if(new_features):
-        preproc.make_features(conf_dict)
+        preproc.make_features(conf_dict,weight=0.25)
     img_path=conf_dict['img_path']
     feat_path=conf_dict['feat_path']
-    seq.features.extract_features(img_path,feat_path,'seq',True)
+    seq.features.extract_features(img_path,feat_path,'seq',short_names)
+
+def easy_make_seq(conf_path,new_feat=True):
+    make_seq_files(conf_path, new_feat, not new_feat)
 
 if __name__ == "__main__":
     conf_path="conf/dane3.cfg"
-    make_seq_files(conf_path, new_features=False)
+    easy_make_seq(conf_path, new_feat=False)
