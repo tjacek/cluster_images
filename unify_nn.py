8ce9a18b6073b99c0717fdc8a15c9d06af733298
tjacek <tjacek@student.agh.edu.pl>
2016-12-01 23:19:14 +0100

    weights

--- unify_nn.py
+++ unify_nn.py
@@ -3,10 +3,18 @@ import deep.reader as reader
 import basic.combine 
 import utils.conf
 import cats
+import preproc
 
-def make_feat_files(conf_paths):
-    for conf_path_i in conf_paths:
-        cats.easy_make_seq(conf_path_i, new_feat=True)
+def make_feat_files(conf_paths,weights=None):
+    if(weights==None):
+        for conf_path_i in conf_paths:
+            #preproc.make_features(conf_path_i)
+            cats.easy_make_seq(conf_path_i, new_feat=True)
+    else:
+        for conf_path_i,weight_i in zip(conf_paths,weights):
+            #preproc.make_features(conf_path_i,weight_i)
+            cats.easy_make_seq(conf_path_i, new_feat=True,weight=weight_i)
+
 
 def combine_feat(conf_paths,out_path):
     conf_files=[ utils.conf.read_config(conf_path_i)
@@ -24,9 +32,10 @@ def make_combined_seq(feat_path,img_path):
 
 if __name__ == "__main__":
     conf_paths=['conf/exp1.cfg','conf/exp2.cfg']	
+    weights=[1.0,1.0]
     out_path='../exp3'
     img_path='../exp3/cats'
-    #make_feat_files(conf_paths)
+    make_feat_files(conf_paths,weights)
     feat_path=out_path+'/feat.txt'
-    #combine_feat(conf_paths,feat_path)
+    combine_feat(conf_paths,feat_path)
     make_combined_seq(feat_path,img_path)
