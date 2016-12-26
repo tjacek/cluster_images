b3d8bb7f7f2092c278fda6996cba20173d3e9cdd
tjacek <tjacek@student.agh.edu.pl>
2016-12-26 23:07:18 +0000

    compute gini index

--- ensemble/dispersion.py
+++ ensemble/dispersion.py
@@ -1,18 +1,23 @@
 import numpy as np 
 
 def gini_index(in_vector):
-    in_vector=sort_vector(in_vector)
-    print(in_vector)
-    n=float(len(in_vector))
-    gini_value=sum([(2.0*i - n - 1.0)*y_i
-                    for i,y_i in enumerate(in_vector)])  
-    gini_value/= n * sum(in_vector)
-    return gini_value
+    f=sort_vector(in_vector)
+    n=len(in_vector)
+    y=range(1,n+1)
+    f_sum = gini_sum(f,n)
+    s_sum=[f_sum[i-1]+  f_sum[i]
+            for i in range(1,n+1)]
+    return 1.0-dot_product(f,s_sum)   
 
-def gini_sum(f,y):
-    s_array=sum([ f_i * y_i 
+def gini_sum(f,n):
+    f_sum=[0]
+    for i in range(n):
+        f_sum.append( f_sum[-1] + f[i] )
+    return f_sum
+
+def dot_product(f,y):
+    return sum([ f_i * y_i 
                   for f_i,y_i in zip(f,y)])
-    return s_array
     
 def sort_vector(in_vector):
     if(type(input)!=list):
@@ -21,6 +26,6 @@ def sort_vector(in_vector):
     return in_vector#np.array(in_vector)
 
 if __name__ == "__main__":
-    test=[4.0,1.0,7.0,5.0,3.0,2.0]
+    test=[0.25,0.25,0.25,0.25]
     sort_vec=sort_vector(test)
     print(gini_index(sort_vec))
