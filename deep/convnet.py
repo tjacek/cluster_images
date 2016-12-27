a3d67ada20d66f84bd424869c6077b981fd5fe51
tjacek <tjacek@student.agh.edu.pl>
2016-12-27 22:42:16 +0000

    improve L1 regularization API

--- deep/convnet.py
+++ deep/convnet.py
@@ -106,24 +106,27 @@ def get_updates(loss,out_layer):
     return updates
 
 def default_params():
-    return {"input_shape":(None,2,60,60),"num_filters":16,"n_hidden":100,
+    return {"input_shape":(None,3,60,60),"num_filters":16,"n_hidden":100,
               "filter_size":(5,5),"pool_size":(4,4),"p":0.5}
 
-def get_model(preproc,nn_path=None,compile=True):
+def get_model(preproc,nn_path=None,compile=True,l1_reg=True):
     if(nn_path==None):
         compile=True
     if(compile):
         params=default_params()
         params['n_cats']= data.get_n_cats(y)
-        return compile_convnet(params,preproc,False)
+        return compile_convnet(params,preproc,l1_reg=l1_reg)
     else:
         nn_reader=deep.reader.NNReader(preproc)
         return nn_reader(nn_path,0.1)
 
+#def train_convnet():
+
+
 if __name__ == "__main__":
-    img_path='../dataset2/exp1/train'
-    nn_path='../dataset2/exp1/nn_full_nol2'
-    preproc=tools.ImgPreproc2D()
+    img_path='../dataset1/exp2/train_trivial'
+    nn_path='../dataset1/exp2/nn_trivial'
+    preproc=tools.ImgPreprocProj()
     imgset=imgs.make_imgs(img_path,norm=True)
     print("read")
     print(len(imgset))
