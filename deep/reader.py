321fe03a5ea873eaf40dba7a864fa5e0c088e040
tjacek <tjacek@student.agh.edu.pl>
2017-01-03 12:27:50 +0000

    refactore reader

--- deep/reader.py
+++ deep/reader.py
@@ -2,18 +2,22 @@ import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import deep
 import pickle
-from deep.convnet import compile_convnet
-from deep.ae import compile_autoencoder
-from deep.autoconv import compile_conv_ae
-from deep.lstm import compile_lstm
+import deep.convnet 
+import deep.ae 
+import deep.autoconv
+import deep.lstm
+#from deep.convnet import compile_convnet
+#from deep.ae import compile_autoencoder
+#from deep.autoconv import compile_conv_ae
+#from deep.lstm import compile_lstm
 
 class NNReader(object):
     def __init__(self,preproc=None):
         self.preproc=preproc
-        self.types = {'Convet':compile_convnet,
-                      'Autoencoder':compile_autoencoder,
-                      'ConvAutoencoder':compile_conv_ae,
-                      'LSTM':compile_lstm}
+        self.types = {'Convet':deep.convnet.compile_convnet,
+                      'Autoencoder':deep.ae.compile_autoencoder,
+                      'ConvAutoencoder': deep.autoconv.compile_conv_ae,
+                      'LSTM':deep.lstm.compile_lstm}
 
     def __call__(self,in_path, drop_p=0.0):
         model=self.__unpickle__(in_path) 
