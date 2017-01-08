a90260a4c9479e100c113118dcb28f6e47ffb19e
tjacek <tjacek@student.agh.edu.pl>
2017-01-08 23:24:35 +0000

    make ensemble

--- ensemble/__init__.py
+++ ensemble/__init__.py
@@ -1,4 +1,5 @@
 import numpy as np
+import single_cls as s_cls
 
 class Ensemble(object):
     def __init__(self,simple_cls):
@@ -12,5 +13,12 @@ class Ensemble(object):
         return np.argmax(dist)
 
 def make_ensemble(conf):
-	simple_cls=[]
-    return Ensemble(simple_cls)	
+    assert(type(conf)==list)
+    simple_cls=[s_cls.make_single_cls(conf_i[1],conf_i[0])
+                  for conf_i in conf]
+    return Ensemble(simple_cls)
+
+if __name__ == "__main__":
+    full=('../dataset1/exp1/lstm_full','../dataset1/exp1/nn_full')
+    f_4=('../dataset1/exp1/lstm_4','../dataset1/exp1/nn_4')
+    ens=make_ensemble([full,f_4])
