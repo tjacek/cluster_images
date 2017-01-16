163419c65a01240f0184fb20dbfa514ea7cc5b4b
tjacek <tjacek@student.agh.edu.pl>
2017-01-16 22:56:50 +0000

    wrap

--- ensemble/wrap.py
+++ ensemble/wrap.py
@@ -1,20 +1,13 @@
 import numpy as np
 
-class FeatSeq(object):
-    def __init__(self,conv):
+class SingleDTW(object):
+    def __init__(self,conv,seqs,k=1):
+        self.seqs=seqs
+        self.k=k
         self.conv=conv
 
     def __call__(self,action):
-        return [self.conv(img_i)  
-                for img_i in action.img_seq]
-
-class DTWcls(object):
-    def __init__(self,seqs,k,metric):
-        self.seqs=seqs
-        self.k=k
-        self.metric=metric
-
-    def __call__(self,x):
+        action_feat=self.conv(action)
         distance=[self.metric(x,seq_i) 
                     for seq_i in self.seqs]
         distance=np.array(distance)
