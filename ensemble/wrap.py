55113b1dc41205ccc9960ceb0f7238cc15d5f661
tjacek <tjacek@student.agh.edu.pl>
2017-01-17 22:48:22 +0000

    scale of dtw dataset

--- ensemble/wrap.py
+++ ensemble/wrap.py
@@ -1,4 +1,5 @@
 import numpy as np
+from seq.dtw import dtw_metric
 
 class SingleDTW(object):
     def __init__(self,conv,seqs,k=1):
@@ -6,9 +7,15 @@ class SingleDTW(object):
         self.k=k
         self.conv=conv
 
+
     def __call__(self,action):
         action_feat=self.conv(action)
         distance=[self.metric(x,seq_i) 
                     for seq_i in self.seqs]
         distance=np.array(distance)
         dist_inds=distance.argsort()[0:k]
+
+    def dataset_scale(self):
+        pair_dist=[dtw_metric(seq_i,seq_j)
+            for seq_i in self.seqs
+              for seq_j in self.seqs]
