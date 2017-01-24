8ac394b71e0fe68bfbea5b47c9b1bf3dffdcd0cd
tjacek <tjacek@student.agh.edu.pl>
2017-01-24 19:01:07 +0000

    save results

--- ensemble/self_rating.py
+++ ensemble/self_rating.py
@@ -2,6 +2,7 @@ import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np
 import ensemble.feat_seq
+import utils.actions 
 
 class HighRatedFrames(object):
     def __init__(self,criterion,threshold=0.9):
@@ -18,7 +19,9 @@ class ConvnetCriterion(object):
 
     def __call__(self,frame_x):
         dist=self.convnet.get_distribution(frame_x)	
-        return np.linalg.norm(dist, ord=2)
+        value=np.linalg.norm(dist, ord=2)
+        print(value)
+        return value
 
 def make_high_rated_frames(nn_path,threshold=0.9,prep_type="time"):
     conv=ensemble.feat_seq.read_convnet(nn_path,prep_type)
@@ -26,5 +29,15 @@ def make_high_rated_frames(nn_path,threshold=0.9,prep_type="time"):
     return HighRatedFrames(crit,threshold)
 
 if __name__ == "__main__":
-    nn_path="../dataset1/exp1/nn_trivial"
-    make_high_rated_frames(nn_path)
+    nn_path="../dataset1/exp1/nn_full"
+    dataset_path="../dataset1/exp1/full_dataset"
+    output_path="../dataset1/exp1/full_dataset_"
+    high_rated=make_high_rated_frames(nn_path)
+    actions=ensemble.feat_seq.read_actions(dataset_path)
+    new_actions=[ action_i(high_rated) 
+                 for action_i in actions]
+    new_actions=[ action_i 
+                  for action_i in actions
+                    if len(action_i) >0 ]
+    utils.actions.save_actions(actions,output_path)                
+    print(len(new_actions))
