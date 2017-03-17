0ff3d52d950ac262388d5d489cac1bbfe4b42a94
tjacek <tjacek@student.agh.edu.pl>
2017-03-17 09:16:36 +0100

    projection class

--- utils/actions/frames.py
+++ utils/actions/frames.py
@@ -57,18 +57,21 @@ class MotionFrames(object):
         return [ self.scale*motion_helper( diff_seq[i],diff_seq[i+1])
                    for i in range(n) ]
 
-def diff_frames(img_seq,threshold=0.15):
+def diff_frames(img_seq,threshold=0.0):
     n=len(img_seq)-1
     def diff_helper(i):
+
         diff_img=np.abs(img_seq[i]-img_seq[i+1])
         #print(diff_img.is_normal())
-        diff_img[ diff_img>threshold]=1.0
-        diff_img[ diff_img<=threshold]=0.0
+        diff_img[ diff_img>=threshold]=1.0
+        #diff_img[ diff_img<threshold]=0.0
         return diff_img
     return [  diff_helper(i)
               for i in range(n)]
 
 def bound_frames(img_seq):
+    #print(type(img_seq))
+    #print([img_i.get_orginal() for img_i in img_seq])
     nonzero= utils.actions.bound.nonzero_frames(img_seq)
     points=  utils.actions.bound.simple_bbox(nonzero)
     extract_box=utils.actions.bound.ExtractBox(points)
@@ -80,25 +83,36 @@ def bound_local(img_seq):
     return [utils.actions.bound.nonzero_double(img_seq[i],img_seq[i+1],True) 
              for i in range(n)]
 
-def proj_xz_frames(img_seq):
-    z_max=max_frames(img_seq)+2
-    def proj_helper(img_i):
-        print(img_i.name)
-        img_i=img_i.get_orginal()
-        proj_xz=np.zeros( (img_i.shape[0],z_max))
-        for (x, y), z in np.ndenumerate(img_i):
-            z=np.floor(z)
-            proj_xz[x][z]=100.0
-        return utils.imgs.Image(img_i.name,proj_xz)
-    return [proj_helper(img_i) 
-               for img_i in img_seq]
 
-def proj_xy_frames(img_seq):
-    def proj_helper(img_i):
-        img_i[img_i!=0]=100.0
-        return img_i
-    return [proj_helper(img_i) 
-               for img_i in img_seq]
+class ProjFrames(object):
+    def __init__(self,zx=True):
+        self.zx=zx
+
+    def __call__(self,img_seq):
+        z_max=max_frames(img_seq)+2
+        def proj_helper(img_i):
+            print(img_i.name)
+            img_i=img_i.get_orginal()
+            proj_xz=self.get_clean_img( img_i,z_max)
+            for (x, y), z in np.ndenumerate(img_i):
+                i,j=self.get_index(x,y,z)
+                proj_xz[i][j]=100.0
+            return utils.imgs.Image(img_i.name,proj_xz)
+        return [proj_helper(img_i) 
+                   for img_i in img_seq]
+
+    def get_clean_img(self, img_i,z_max):
+        if(self.zx):
+            return np.zeros( (img_i.shape[0],z_max))
+        else:
+            return np.zeros( (img_i.shape[1],z_max,))
+
+    def get_index(self,x,y,z):
+        z=np.floor(z)
+        if(self.zx):
+            return x,z
+        else:
+            return y,z
 
 def max_frames(img_seq):
     return max([np.amax(img_i)
