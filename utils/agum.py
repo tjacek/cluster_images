a79036cc085deb29fbb788c510f87bc3757b75f2
tjacek <tjacek@student.agh.edu.pl>
2017-03-22 19:10:56 +0100

    subsamble action

--- utils/agum.py
+++ utils/agum.py
@@ -15,6 +15,18 @@ class FlipAction(object):
         new_action.name= self.agum_name+ '_' + new_action.name
         return new_action
 
+class SubsampleAction(object):
+    def __init__(self):
+        self.agum_name='subsample'
+
+    def __call__(self,action_i):
+        new_seq=[ img_i
+                    for i,img_i in enumerate(action_i.img_seq)
+                      if (i % 2)==0]  
+        new_action=action_i(lambda old_seq:new_seq)              
+        new_action.name= self.agum_name+ '_' + new_action.name
+        return new_action
+
 @utils.paths.path_args
 def agum_actions(action_path,out_path,dataset_format='cp_dataset'):
     action_read=utils.actions.read.ReadActions(str(dataset_format))
@@ -25,31 +37,7 @@ def agum_actions(action_path,out_path,dataset_format='cp_dataset'):
     all_actions=old_actions+new_actions
     utils.actions.read.save_actions(all_actions,out_path)
 
-#def agum_data(action_path,out_path):
-#    old_actions=actions.read_actions(action_path)
-#    new_actions=[flip_action(action_i)
-#                  for action_i in old_actions]
-#    all_actions=old_actions+new_actions
-#    dirs.make_dir(out_path)
-#    for action_i in all_actions:
-#        path_i=utils.paths.Path(out_path)
-#        path_i.add(action_i.cat)
-#        dirs.make_dir(path_i)
-#        action_i.save(str(path_i))
-
-#def subsample_action(action_i):
-#    name=action_i.name+"_h"
-#    cat=action_i.cat
-#    person=action_i.person
-#    img_seq=[ img_i
-#              for i,img_i in enumerate(action_i.img_seq)
-#                if (i%2)==0]
-#    return actions.Action(name,img_seq,cat,person)
-
 if __name__ == "__main__":
     in_path='../dataset2a/exp3/train'
     out_path='../dataset2a/exp3/train_agum'
     agum_actions(in_path,out_path,'basic_dataset')
-    #in_path='../dataset2a/train/'
-    #out_path='../dataset2a/train_agum/'
-    #agum_data(in_path,out_path)
