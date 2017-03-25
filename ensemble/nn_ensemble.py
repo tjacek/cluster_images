ddaac1b4de90d5e3ef8f0fa7bfdfd1e790ccb1ed
tjacek <tjacek@student.agh.edu.pl>
2017-03-25 12:33:43 +0100

    simple nn ensemble

--- ensemble/nn_ensemble.py
+++ ensemble/nn_ensemble.py
@@ -5,6 +5,25 @@ from ensemble.single_cls import make_single_cls
 import utils.actions
 import numpy as np
 
+class SimpleNNEnsemble(object):
+    def __init__(self,nnetworks):
+        self.nnetworks=nnetworks
+
+    def __call__(self,action_i):
+        dists=[ nn_j(action_i)
+                for nn_j in self.nnetworks]
+        dists=np.array(dists)
+        print(dists.shape)
+        return np.sum(dists,axis=0)
+    
+    def get_category(self,action_i):
+        return np.argmax(self(action_i))
+
+def make_simple_nn(lstm_paths,conv_paths):
+    nn=[ make_single_cls(conv_path_i,lstm_path_i)
+         for lstm_path_i,conv_path_i in zip(lstm_paths,conv_paths)]
+    return SimpleNNEnsemble(nn)
+
 class MultiNNEnsemble(object):
     def __init__(self,datasets,nnetworks,dispersion=False,vote=False):
         self.datasets=datasets
@@ -27,7 +46,8 @@ class MultiNNEnsemble(object):
             return [ get_binary_dist(dist_i) 
                       for dist_i in distributions]
         if(dispersion):
-            return [ np.linalg.norm(dist_i,ord=2) * dist_i 
+            return [ #L2(dist_i) *
+                     dist_i 
                       for dist_i in distributions]
         else:    
             return distributions    
@@ -40,6 +60,9 @@ class MultiNNEnsemble(object):
         typeed_nn=self.nnetworks[type_i]
         return typeed_nn(typed_action)
 
+def L2(dist_i):
+    return np.linalg.norm(dist_i,ord=2)
+
 def get_binary_dist(dist):
     index=np.argmax(dist)
     binary_dist= np.zeros(dist.shape)
@@ -51,7 +74,7 @@ def make_multi_nn(dataset_paths,nn_paths):
         raise("type:dataset_paths dict required")
     if(type(nn_paths)!=dict):
         raise("type:nn_paths dict required")
-    datasets=make_datasets(dataset_paths)
+    datasets=make_datasets(dataset_paths,s_action=None)
     nnetworks={type_i:make_single_cls(conv_path=path_pair_i[0],lstm_path=path_pair_i[1],prep_type=type_i)
                   for type_i,path_pair_i in nn_paths.items()}
     return MultiNNEnsemble(datasets,nnetworks)
@@ -66,14 +89,16 @@ def make_actions_dict(path_i,s_action='odd'):
                for action_i in actions}
 
 if __name__ == "__main__":
-    dataset_paths={'time':'../dataset1/exp1/full_dataset',
-                   'proj':'../dataset1/exp2/cats'}
-    nn_paths={'time':('../dataset1/exp1/nn_data_1' ,'../dataset1/exp1/lstm_self'),
-              'proj':('../dataset1/exp2/nn_worst' ,'../dataset1/exp2/lstm_worst')}
-    ens=make_multi_nn(dataset_paths,nn_paths)
+    conv_paths=['../ensemble/basic_nn/nn_basic','../ensemble/18_nn/nn_18','../ensemble/16_nn/nn_16']
+    lstm_paths=['../ensemble/basic_nn/lstm_basic','../ensemble/18_nn/lstm_18','../ensemble/16_nn/lstm_16']
 
-    in_path="../dataset1/exp1/full_dataset"
-    read_actions= utils.actions.ReadActions( utils.actions.cp_dataset,norm=True)
-    actions=read_actions(in_path)
-    s_actions= utils.actions.select_actions(actions)
+    ens=make_simple_nn(lstm_paths,conv_paths)
+    #dataset_paths={'time':'../dataset1/exp1/full_dataset',
+    #               'proj':'../dataset1/exp2/cats'}
+    #nn_paths={'time':('../ensemble/exp1/nn_data_1' ,'../dataset1/exp1/lstm_self'),
+    #          'proj':('../dataset1/exp2/old/nn_worst' ,'../dataset1/exp2/old/lstm_worst')}
+    #ens=make_multi_nn(dataset_paths,nn_paths)
+
+    in_path='../ensemble/full'
+    s_actions=utils.actions.apply_select(in_path,action_type='even',norm=True)
     ensemble.check_model(ens,s_actions)
--- ensemble/single_cls.py
+++ ensemble/single_cls.py
@@ -30,19 +30,9 @@ class SingleCls(object):
         for i in range(seq_diff):
             seq_feats.append(np.zeros((self.seq_dim,)))
         masked_feats=np.array(seq_feats)
+
         mask=make_mask(seq_len,self.max_seq)
         return self.lstm.get_distribution(masked_feats,mask)
-        #seq_feats_arr=np.array(seq_feats)
-        
-        #seq_size=seq_feats_arr.shape[0]
-        #seq_dim=seq_feats_arr.shape[1]
-        
-        #print(seq_size)
-        #print(seq_dim)
-        #masked_feats=masked_seq(seq_feats_arr,seq_size,self.max_seq,seq_dim)
-        #mask=make_mask(seq_size,self.max_seq)
-        #result=self.lstm.get_distribution(masked_feats,mask)                	
-        #return result
 
     def get_category(self,action):
         return np.argmax(self(action))
@@ -59,7 +49,7 @@ def masked_seq(seq_i,seq_i_len,max_seq,seq_dim):
     new_seq_i[:seq_i_len]=seq_i[:seq_i_len]
     return new_seq_i
 
-def make_single_cls(conv_path,lstm_path,with_type=False,prep_type="time"):
+def make_single_cls(conv_path,lstm_path,with_type=False,prep_type="proj"):
     seq_feat=ensemble.feat_seq.make_feat_seq(conv_path,prep_type)
     nn_reader=deep.reader.NNReader(deep.reader.get_preproc(prep_type))
     lstm,hyperparams=nn_reader(lstm_path,drop_p=0.0,get_hyper=True)
@@ -69,10 +59,13 @@ def make_single_cls(conv_path,lstm_path,with_type=False,prep_type="time"):
     return single_cls
 
 if __name__ == "__main__":
-    conv_path='../dataset1/exp2/nn_worst'
-    cat_path='../dataset1/exp2/cats'
-    lstm_path='../dataset1/exp2/lstm_worst'
-    single_cls=make_single_cls(conv_path,lstm_path,prep_type='proj')
+    conv_path='../dataset1/exp1/nn_data_1'
+    cat_path='../dataset1/exp1/full_dataset'
+    #lstm_path='../dataset1/exp2/lstm_worst'
+    #single_cls=make_single_cls(conv_path,lstm_path,prep_type='proj')
     actions=ensemble.read_actions(cat_path)
-    s_actions= utils.actions.select_actions(actions)
-    ensemble.check_model(single_cls,s_actions)
+    print(actions[0].name)
+    seq_feat=ensemble.feat_seq.make_feat_seq(conv_path,prep_type='time')
+    print(seq_feat(actions[0]))
+    #s_actions= utils.actions.select_actions(actions)
+    #ensemble.check_model(single_cls,s_actions)
