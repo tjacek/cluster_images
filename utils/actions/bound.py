cd7d2ac19755954cfc561d2b9dabf0909ed42407
tjacek <tjacek@student.agh.edu.pl>
2017-04-01 23:49:15 +0200

    remove coments

--- utils/actions/bound.py
+++ utils/actions/bound.py
@@ -14,7 +14,7 @@ class ExtractBox(object):
     	print(self.start_point)
     	print(self.end_point)
         new_img1=org_img1[self.start_point[1]:self.end_point[1],self.start_point[0]:self.end_point[0]]
-        return utils.imgs.Image(img1.name,new_img1)#,img1.org_dim)
+        return utils.imgs.Image(img1.name,new_img1)
 
 def nonzero_frames(img_seq):
     first=img_seq[0]
@@ -43,8 +43,6 @@ def make_extract_box(nonzero_frame):
     return ExtractBox(points)
 
 def simple_bbox(nonzero_frames):
-    #if(typenonzero_frames):
-    #nonzero_frames= nonzero_frames.get_orginal()
     nonzero_frames=nonzero_frames.astype(np.uint8)
     x0,y0,w,h=cv2.boundingRect(nonzero_frames)
     return [(x0,y0),((x0+w,y0+h))] 
