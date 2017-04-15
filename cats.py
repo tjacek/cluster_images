7b859f35277ecf674c48d2291b5683ab3dde54ec
tjacek <tjacek@student.agh.edu.pl>
2017-04-15 18:33:31 +0100

    configure output path

--- cats.py
+++ cats.py
@@ -2,6 +2,7 @@ import preproc
 import seq.features
 import utils.conf
 import utils.actions
+import utils.paths
 
 def make_seq_files(conf_path, new_features=True,short_names=True,weight=0.25):
     if(type(conf_path)==str):
@@ -12,12 +13,18 @@ def make_seq_files(conf_path, new_features=True,short_names=True,weight=0.25):
         preproc.make_features(conf_dict,weight)
     img_path=conf_dict['img_path']
     feat_path=conf_dict['feat_path']
-    seq.features.extract_features(img_path,feat_path,'seq',short_names)
+
+    out_path=get_output_path(conf_path)
+    seq.features.extract_features(img_path,feat_path,out_path,short_names)
 
 def easy_make_seq(conf_path,new_feat=True,weight=1.00):
     print(conf_path)
     make_seq_files(conf_path, new_feat, not new_feat,weight)
 
+def get_output_path(conf_path,input_path='nn_path',dir_name='seq'):
+    raw_path=utils.paths.Path(conf_dict[input_path])
+    return raw_path.copy().set_name(dir_name)
+
 if __name__ == "__main__":
-    conf_path="conf/1exp1b.cfg"
+    conf_path="conf/set10.cfg"
     easy_make_seq(conf_path, new_feat=True)
--- seq/features.py
+++ seq/features.py
@@ -2,16 +2,21 @@ import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np
 import utils.paths as paths
-import utils.dirs as dirs
+import utils.paths.dirs as dirs
 import utils.imgs as imgs
-import utils.files as files
+import utils.paths.files as files
 import utils.pcloud as pcloud
 import basic
 import basic.external as ext
+        
+#def transform_external(in_file,out_file,short_names,dataset_format='cp_dataset'):
+#    extractor=ext.read_external(str(in_path),short_names) 
+#    action_reader=utils.actions.read.ReadActions(dataset_format)
 
 @paths.path_args
-def extract_features(in_path,ext_path,seq_path='seq',short_names=True):
-    out_path=in_path.copy().set_name(str(seq_path))
+def extract_features(in_path,ext_path,out_path,
+                                      short_names=True):
+    #out_path=in_path.copy().set_name(str(seq_path))
     dirs.make_dir(str(out_path))
     extractor=ext.read_external(str(ext_path),short_names)
     transform_seq(in_path,out_path,extractor)
