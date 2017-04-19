a0f87ccfb1dfe013a0457ede70576b1328f062cf
tjacek <tjacek@student.agh.edu.pl>
2017-04-19 23:09:04 +0100

    to dataset

--- feat/__init__.py
+++ feat/__init__.py
@@ -2,4 +2,12 @@ def action_pairs(actions):
     pairs=[]
     for action_i in actions:
         pairs+=action_i.to_pairs()
-    return pairs	
+    return pairs
+
+def to_dataset(pairs):
+    y=[ pair_i[0] 
+        for pair_i in pairs]
+    X=[ pair_i[1] 
+        for pair_i in pairs]
+    X=np.array(X)
+    return X,y	
