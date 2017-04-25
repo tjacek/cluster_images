4cd7682284c174d043c69d5504bbba779c904aae
tjacek <tjacek@student.agh.edu.pl>
2017-04-25 14:12:49 +0200

    refactore extract numbers

--- utils/text.py
+++ utils/text.py
@@ -1,6 +1,17 @@
 import re
 import paths.files 
 
+class ExtractNumber(object):
+    def __init__(self, use_path=False):
+        self.pattern=re.compile(r"\d+")
+        self.use_path=use_path
+
+    def __call__(self,name):
+        if(self.use_path):
+            name=name.get_name()
+        raw_digits=re.findall(self.pattern,str(name))
+        return int(raw_digits[0]) 
+
 def get_person(name):
     raw=name.split('_')[0]
     return extract_number(raw)
@@ -9,10 +20,10 @@ def get_category(name):
     raw=name.split('_')[0]
     return extract_number(raw)
     
-def extract_number(name):
-    pattern = re.compile(r"\d+")
-    raw_digits=re.findall(pattern,str(name))[0]
-    return int(raw_digits)
+#def extract_number(name):
+#    pattern = re.compile(r"\d+")
+#    raw_digits=re.findall(pattern,str(name))
+#    return int(raw_digits[0])
 
 def extract_sufix(filename):
     return filename.split(".")[-1] 
