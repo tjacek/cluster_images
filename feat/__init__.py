195a2b237c5891569d834eea2475f586a2d24faf
tjacek <tjacek@student.agh.edu.pl>
2017-04-26 19:21:37 +0200

    to dataset

--- feat/__init__.py
+++ feat/__init__.py
@@ -19,24 +19,40 @@ def action_pairs(actions):
         pairs+=action_i.to_pairs(i)
     return pairs
 
-def to_dataset(pairs):
-    def data_helper(k):
-        return [ pair_i[k] 
-                 for pair_i in pairs]
-    y= data_helper(0)
-    X= data_helper(1)
-    X=np.array(X)
-    return X,y	
+class ToDataset(object):
+    def __init__(self, dataset_format=None):
+        if(dataset_format==None):
+            self.dataset_format=utils.actions.read.cp_dataset
+        else:
+            self.dataset_format=dataset_format
+
+    def __call__(self,feat_dir):
+        X=[]
+        y=[]
+        names=[]
+        for key_i,value_i in feat_dir.items(True):
+            print(key_i)
+            action_i = feat_dir.extract_action(key_i)
+            name,cat,person=self.dataset_format(action_i)
+            X.append(value_i)
+            y.append(int(cat))
+            names.append(key_i)
+        X=np.array(X)
+        return X,y,names
+
+def transform_feat(in_path,out_path,dataset_format='cp_dataset'):
+    feat_dict=basic.external.read_external(in_path)
+    action_dict=feat_dict.divided_by_action()
+    new_actions=utils.actions.read.NewActions(dataset_format)
+    actions=new_actions(action_dict)
+    save_actions=utils.actions.read.SaveActions(img_actions=False)
+    save_actions(actions,out_path)
 
 if __name__ == "__main__":
     in_path='../ensemble/basic_nn/feat.txt'
     out_path='../ensemble/basic_nn/seq2'
     feat_dict=basic.external.read_external(in_path)
-    action_dict=feat_dict.divided_by_action()
-    new_actions=utils.actions.read.NewActions('cp_dataset')
-    actions=new_actions(action_dict)
-    save_actions=utils.actions.read.SaveActions(img_actions=False)
-    save_actions(actions,out_path)
-
-
+    to_dataset=ToDataset()
+    to_dataset(feat_dict)
+    #transform_feat(in_path,out_path)
     #print(feat_dict.divided_by_action())
