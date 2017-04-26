243dac55f66760182ffa0be3bd3a527d7ee2cd6e
tjacek <tjacek@student.agh.edu.pl>
2017-04-27 00:10:29 +0200

    full feature selection

--- feat/__init__.py
+++ feat/__init__.py
@@ -4,20 +4,27 @@ import numpy as np
 import utils.actions,utils.actions.read
 import basic.external
 from sklearn.feature_selection import SelectFromModel
+from sklearn import linear_model
 
-def lasso_model(X,y):
+def reduce_feat(in_path,model):
+    feat_dict=basic.external.read_external(in_path)
+    to_dataset=ToDataset()
+    X,y,names=to_dataset(feat_dict)
+    new_X=model(X,y,True)
+    print(new_X.shape)
+    to_feat_dict=ToFeatDict()
+    new_feat_dict=to_feat_dict(new_X,names)
+    return new_feat_dict
+
+def lasso_model(X,y,transform=False):
     clf = linear_model.Lasso(alpha=0.1)
     clf.fit(X,y)
     #lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X, y)
     model = SelectFromModel(clf, prefit=True)
-    return model
-#    X_new = model.transform(X)
-
-def action_pairs(actions):
-    pairs=[]
-    for i,action_i in enumerate(actions):
-        pairs+=action_i.to_pairs(i)
-    return pairs
+    if(transform):
+        return model.transform(X)
+    else:
+        return model
 
 class ToFeatDict(object):
     def __init__(self,  dataset_format=None):
@@ -27,8 +34,9 @@ class ToFeatDict(object):
             self.dataset_format=dataset_format
         
     def __call__(self,X,names):
-        return { name_i:X[i]
+        raw_dict={ name_i:X[i]
                   for i,name_i in enumerate(names)}
+        return basic.external.ExternalFeats( raw_dict)
 
 class ToDataset(object):
     def __init__(self, dataset_format=None):
@@ -51,27 +59,25 @@ class ToDataset(object):
         X=np.array(X)
         return X,y,names
 
-def transform_feat(in_path,out_path,dataset_format='cp_dataset'):
-    feat_dict=basic.external.read_external(in_path)
+def action_feat(feat_dict,out_path,dataset_format='cp_dataset'):
     action_dict=feat_dict.divided_by_action()
     new_actions=utils.actions.read.NewActions(dataset_format)
     actions=new_actions(action_dict)
     save_actions=utils.actions.read.SaveActions(img_actions=False)
     save_actions(actions,out_path)
-
+    
 if __name__ == "__main__":
     in_path='../ensemble/basic_nn/feat.txt'
     out_path='../ensemble/basic_nn/seq2'
-    feat_dict=basic.external.read_external(in_path)
-    to_dataset=ToDataset()
-    X,y,names=to_dataset(feat_dict)
+    new_dict=reduce_feat(in_path,lasso_model)
+    action_feat(new_dict,out_path)
+    #feat_dict=basic.external.read_external(in_path)
+    #to_dataset=ToDataset()
+    #X,y,names=to_dataset(feat_dict)
 
-    to_feat_dict=ToFeatDict()
-    new_feat_dict=to_feat_dict(X,names)
+    #to_feat_dict=ToFeatDict()
+    #new_feat_dict=to_feat_dict(X,names)
 
-    key_1=feat_dict.raw_dict.keys()[0]
-    print(feat_dict[key_1])
-    print("$$$$$$$$$$$$$$$$$$$$$$$$")
-    print(new_feat_dict[key_1])
+    #key_1=feat_dict.raw_dict.keys()[0]
     #transform_feat(in_path,out_path)
     #print(feat_dict.divided_by_action())
