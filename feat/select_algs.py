f82880072ddbef5240e44e465bcf00f01061e3bf
tjacek <tjacek@student.agh.edu.pl>
2017-05-08 19:55:32 -0500

    tree selection

--- feat/select_algs.py
+++ feat/select_algs.py
@@ -10,3 +10,12 @@ def lasso_model(X,y,transform=False):
         return model.transform(X)
     else:
         return model
+
+def tree_model(X,y,transform=False):
+    clf = linear_model.ExtraTreesClassifier()
+    clf.fit(X,y)
+    model = SelectFromModel(clf, prefit=True)
+    if(transform):
+        return model.transform(X)
+    else:
+        return model
