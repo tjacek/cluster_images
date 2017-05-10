680464150658cd87cd16f1f4008c6a90bc3be5d5
tjacek <tjacek@student.agh.edu.pl>
2017-05-10 16:16:00 -0500

    get_paths function

--- seq/__init__.py
+++ seq/__init__.py
@@ -9,6 +9,7 @@ import deep.lstm
 from sklearn.metrics import classification_report,confusion_matrix
 import utils.data
 import deep.reader
+import split
 
 def make_model(train_dataset,make=True,n_epochs=0,p=0.5):
     if(make):
@@ -28,7 +29,7 @@ def check_model(model,test_dataset):
     mask=test_dataset['mask']
     y_pred=[model.get_category(x_i,mask[i])
               for i,x_i in enumerate(x)]
-    print(utils.data.find_errors(y_pred,test))
+    print(utils.data.find_errors(y_pred,test_dataset))
     check_prediction(y_pred,y_true)
 
 class CorrectCond(object):
@@ -68,6 +69,7 @@ def L2(dists_i):
     return np.linalg.norm(dists_i,ord=2)
 
 def train_model(model,dataset,epochs=10000):
+    print(type(dataset))
     x=get_batches(dataset['x'])
     y=get_batches(dataset['y'])
     print(dataset.keys())
@@ -94,16 +96,41 @@ def get_batches(x,batch_size=6):
     return [x[i*batch_size:(i+1)*batch_size] 
                for i in range(n_batches)]
 
-if __name__ == "__main__":
-    path='../dataset2a/exp2/seq'
-    nn_path='../dataset2a/exp2/lstm_full'
-    #path='../ensemble/feat'
-    #nn_path='../ensemble/lstm'
-    dataset=to_dataset.seq_dataset(path,dataset_format='basic_dataset')
+def get_paths(dir_path,nn,seq='seq',lstm='lstm'):
+    path=dir_path + nn+seq 
+    nn_path=dir_path + nn+lstm 
+    return path,nn_path
+
+def create_dataset(path,nn_path):
+
+    split_dataset= split.get_dataset(0,'modulo')
+
+    dataset=to_dataset.seq_dataset(path,dataset_format='cp_dataset')
     print(dataset.params())
     new_dataset=to_dataset.masked_dataset(dataset)
-    train,test=split.person_dataset(new_dataset)
-    model=make_model(train,False,n_epochs=20,p=0.5)
+
+
+    train,test=split_dataset(new_dataset)#split.person_dataset(new_dataset)
+    model=make_model(train,True,n_epochs=100,p=0.0)
     model.get_model().save(nn_path)    
     check_model(model,test)
-    #check_distribution(model,test)
+
+if __name__ == "__main__":
+    #path='../cross/1_set/seq' #'../dataset2a/exp2/seq'
+    #nn_path='../cross/1_set/lstm_full' #'../dataset2a/exp2/lstm_full'
+    path='../ensemble3/12_nn/seq'
+    nn_path='../ensemble3/12_nn/lstm'
+
+    path,nn_path=get_paths('../ensemble3/','select/',seq='seq',lstm='lstm')
+    create_dataset(path,nn_path)
+#    split_dataset= split.get_dataset(0,'modulo')
+
+#    dataset=to_dataset.seq_dataset(path,dataset_format='cp_dataset')
+#    print(dataset.params())
+#    new_dataset=to_dataset.masked_dataset(dataset)
+
+
+#   train,test=split_dataset(new_dataset)#split.person_dataset(new_dataset)
+#    model=make_model(train,True,n_epochs=20,p=0.0)
+#    model.get_model().save(nn_path)    
+#    check_model(model,test)
