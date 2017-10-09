b2a941884a701b1bc373b75d6b36773dad77fc16
tjacek <tjacek@student.agh.edu.pl>
2017-10-09 23:29:41 +0200

    preproc global features

--- preproc.py
+++ preproc.py
@@ -2,9 +2,12 @@ import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import utils.actions.read
 import deep.tools
-import basic
+import basic,seq.dtw_feat
 
-def extract_feats(in_path,out_path,dataset_format='mhad_dataset'):
+def global_feats(seq_path,out_path,dataset_format='mhad_dataset'):
+    seq.dtw_feat.make_dtw_feat(seq_path,out_path,dataset_format=dataset_format)
+
+def local_feats(in_path,out_path,dataset_format='mhad_dataset'):
     read_actions=utils.actions.read.ReadActions(dataset_format)
     actions=read_actions(in_path)
     extractor=select_extractor(extractor_type='basic')
@@ -30,6 +33,7 @@ def select_preproc(preproc):
         return deep.tools.ImgPreproc1D()
     raise Exception("No preproc")
 
-in_path="../../AArtyk2/time/"
-out_path="../../AArtyk2/corel/"
-extract_feats(in_path,out_path)
+seq_path="../../AArtyk2/basic/corel/seq"
+out_path="../../AArtyk2/basic/corel/dtw_feats.txt"
+global_feats(seq_path,out_path)
+#extract_feats(in_path,out_path)
