de9ba5445f5935825358d32554af52a36e98a8f9
tjacek <tjacek@student.agh.edu.pl>
2017-12-26 15:26:41 -0700

    add binary sequence

--- ensemble/binary.py
+++ ensemble/binary.py
@@ -2,19 +2,31 @@ import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np
 import deep.reader,deep.tools as tools
+import utils.actions.read
 
 class BinaryEnsemble(object):
     def __init__(self,binary_networks):
         self.binary_networks=binary_networks
 
     def get_seq(self,actions):
-        return [action_i.transform(self) 
-                    for action_i in self.actions]    	
+        return [action_i.transform(self,img_seq=False) 
+                    for action_i in actions]    	
     
     def __call__(self,img_i):
-        feats=[nn_j.get_distribution(img_i)[0] 
+        print(img_i.shape)
+        feats=[nn_j.get_distribution(img_i)[1] 
                 for nn_j in self.binary_networks]
         return np.array(feats)
+
+def ensemble_seq(in_path,nn_dir,out_path,dataset_format='cp_dataset'):
+    action_reader=utils.actions.read.ReadActions(dataset_format,img_seq=True)
+    actions=action_reader(in_path)
+    for action_i in actions:
+        print(action_i)
+    binary_ensemble=read_ensemble(nn_dir)
+    new_actions=binary_ensemble.get_seq(actions)
+    save_actions=utils.actions.read.SaveActions(img_actions=False)
+    save_actions(new_actions,out_path)
 
 def read_ensemble(nn_dir):
     preproc=tools.ImgPreproc2D()
@@ -29,4 +41,7 @@ def read_ensemble(nn_dir):
     return BinaryEnsemble(nn_models)
 
 if __name__ == "__main__":
-    read_ensemble("../../AArtyk/all_models/")
+    in_path="../../AArtyk/MSR/time/"   
+    nn_dir="../../AArtyk/all_models/"
+    out_path="../../AArtyk_exp/binary"
+    ensemble_seq(in_path,nn_dir,out_path)
