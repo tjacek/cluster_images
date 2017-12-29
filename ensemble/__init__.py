1ecf053afab4e297a2c7d57be908ed113a78048e
tjacek <tjacek@student.agh.edu.pl>
2017-12-28 17:49:47 -0700

    read models

--- ensemble/__init__.py
+++ ensemble/__init__.py
@@ -1,60 +1,12 @@
-import numpy as np
-import single_cls as s_cls
-import utils.actions
-import seq
+import deep.reader,deep.tools as tools
 
-class Ensemble(object):
-    def __init__(self,simple_cls):
-        self.simple_cls=simple_cls
-
-    def __call__(self):
-        return [cls_i(action) 
-                 for cls_i in self.simple_cls] 
-
-    def get_category(self,action):
-        result=self(action) 
-        result=np.array(result)
-        dist=np.sum(result,axis=0)
-        return dist.argmax()
-
-def make_ensemble(conf):
-    assert(type(conf)==list)
-    simple_cls=[s_cls.make_single_cls(conf_i[1],conf_i[0])
-                  for conf_i in conf]
-    return Ensemble(simple_cls)
-
-def check_model(model,actions):
-    #print(type(actions[0]))	
-    y_true=[int(action_i.cat)-1 for action_i in actions]
-    y_pred=[model.get_category(action_i)
-              for action_i in actions]
-    #print(utils.data.find_errors(y_pred,test))
-    seq.check_prediction(y_pred,y_true)
-
-def test_model(model,action_path,action_selection=None):
-    actions=read_actions(action_path,action_selection=action_selection)
-    #s_actions= utils.actions.select_actions(actions)
-    check_model(model,actions)
-
-def read_actions(cat_path,action_type='cp_dataset', action_selection=None):
-    action_reader=utils.actions.ReadActions(action_type,norm=True)
-    actions=action_reader(cat_path)
-    if(len(actions)==0):
-        raise Exception("No actions found in: "+ cat_path)
-    if(action_selection!=None):
-        selected_actions=utils.actions.select_actions(actions,action_selection)
-        if(len(actions)==0):
-            raise Exception("No actions selected "+ action_selection)
-        return selected_actions
-    return actions
-
-if __name__ == "__main__":
-    full=('../dataset1/exp1/lstm_full','../dataset1/exp1/nn_full')
-    f_4=('../dataset1/exp1/lstm_4','../dataset1/exp1/nn_4')
-    trivial=('../dataset1/exp1/lstm_trivial','../dataset1/exp1/nn_trivial')
-    ens=make_ensemble([f_4,trivial])
-    in_path="../dataset1/exp1/full_dataset"
-    read_actions= utils.actions.ReadActions( utils.actions.cp_dataset,norm=True)
-    actions=read_actions(in_path)
-    s_actions= utils.actions.select_actions(actions)
-    check_model(ens,s_actions)
+def read_ensemble(nn_dir):
+    preproc=tools.ImgPreproc2D()
+    nn_reader=deep.reader.NNReader(preproc)
+    nn_paths=os.listdir(str(nn_dir))
+    nn_paths=[str(nn_dir)+str(nn_path_i)
+                for nn_path_i in nn_paths]    
+    for nn_i in nn_paths:
+        print(nn_i)
+    return [ nn_reader(nn_i) 
+                for nn_i in nn_paths]
--- ensemble/binary.py
+++ ensemble/binary.py
@@ -1,8 +1,8 @@
 import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np
-import deep.reader,deep.tools as tools
 import utils.actions.read
+import ensemble
 
 class BinaryEnsemble(object):
     def __init__(self,binary_networks):
@@ -25,22 +25,12 @@ class BinaryEnsemble(object):
 def ensemble_seq(in_path,nn_dir,out_path,dataset_format='cp_dataset'):
     action_reader=utils.actions.read.ReadActions(dataset_format,img_seq=True)
     actions=action_reader(in_path)
-    binary_ensemble=read_ensemble(nn_dir)
+    binary_ensemble= BinaryEnsemble( ensemble.read_ensemble(nn_dir))
     new_actions=binary_ensemble.get_seq(actions)
     save_actions=utils.actions.read.SaveActions(img_actions=False)
     save_actions(new_actions,out_path)
 
-def read_ensemble(nn_dir):
-    preproc=tools.ImgPreproc2D()
-    nn_reader=deep.reader.NNReader(preproc)
-    nn_paths=os.listdir(str(nn_dir))
-    nn_paths=[str(nn_dir)+str(nn_path_i)
-                for nn_path_i in nn_paths]    
-    for nn_i in nn_paths:
-    	print(nn_i)
-    nn_models=[ nn_reader(nn_i) 
-                for nn_i in nn_paths]
-    return BinaryEnsemble(nn_models)
+
 
 if __name__ == "__main__":
     in_path="../../AArtyk/MSR/time/"   
