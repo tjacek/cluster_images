e52926b0857b71dcd6fbb47a6a57af100f93f254
tjacek <tjacek@student.agh.edu.pl>
2018-01-01 19:03:20 -0700

    RNNCls

--- ensemble/nn.py
+++ ensemble/nn.py
@@ -2,10 +2,11 @@ import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np
 import ensemble
+import utils.actions.read
 
 class NNEnsemble(object):
-    def __init__(self, deep_nns):
-        self.deep_nns = deep_nns
+    def __init__(self, nns):
+        self.nns = nns
     
     def __call__(self,seqs):
     	return [ self.get_category(seq_i) 
@@ -16,7 +17,7 @@ class NNEnsemble(object):
     
     def get_distribution(self,seq_i):
         dists=[nn_j.get_distribution(seq_i)
-                for nn_j in self.deep_nns]
+                for nn_j in self.nns]
         dists=np.array(dists)
         return np.sum(dists, axis=0)
 
@@ -25,9 +26,9 @@ class RNNCls(object):
         self.rnn=rnn
         self.conv_net=conv_net
     
-    def __call__(self,action):
+    def get_distribution(self,action):
         seq_i=self.get_seq(action)
-        self.rnn.get_distribution(self,x,mask)
+        return self.rnn.get_distribution(seq_i)
 
     def get_seq(self,action):
         if(type(action)!=utils.actions.Action):
@@ -35,11 +36,23 @@ class RNNCls(object):
         return [self.conv(img_i)  
                 for img_i in action.img_seq]
 
+def apply_nn(in_path,rnn,dataset_format='cp_dataset'):
+    action_reader=utils.actions.read.ReadActions(dataset_format,img_seq=True)
+    actions=action_reader(in_path)
+    y=[rnn(action_i) for action_i in actions]
+        
 def read_rnn(rnn_path,conv_path):
     rnns=ensemble.read_ensemble(rnn_path,with_id=True)
     conv_nets=ensemble.read_ensemble(conv_path,with_id=True)
+    clss=[RNNCls(rnns[key_i],conv_nets[key_i]) 
+            for key_i in rnns.keys()]
+    return NNEnsemble(clss)
+
 
 if __name__ == "__main__":
+    in_path="../../AArtyk/MSR/time/"
     rnn_path="../../AArtyk/all_models/"
     conv_path="../../AArtyk_exp/lstm/"
-    read_rnn(rnn_path,conv_path)
+    rnn=read_rnn(rnn_path,conv_path)
+    print("**********************************")
+    apply_nn(in_path,rnn)
