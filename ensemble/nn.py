58429a8cf735fb67028b15a0cbd54d558e731226
tjacek <tjacek@student.agh.edu.pl>
2018-01-02 19:08:19 -0700

    fix bug in ensemble

--- ensemble/nn.py
+++ ensemble/nn.py
@@ -3,15 +3,15 @@ sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np
 import ensemble
 import utils.actions.read
+import seq
 
 class NNEnsemble(object):
     def __init__(self, nns):
         self.nns = nns
     
-    def __call__(self,seqs):
-    	return [ self.get_category(seq_i) 
-    	            for seq_i in seqs]
-    
+    def __call__(self,seq_i):
+    	return self.get_category(seq_i) 
+    	           
     def get_category(self,seq_i):
         return np.argmax(self.get_distribution(seq_i))
     
@@ -27,19 +27,31 @@ class RNNCls(object):
         self.conv_net=conv_net
     
     def get_distribution(self,action):
-        seq_i=self.get_seq(action)
-        return self.rnn.get_distribution(seq_i)
+        seq_i=np.array(self.get_seq(action))
+        max_seq=self.rnn.hyperparams['max_seq']
+        seq_dim=self.rnn.hyperparams['seq_dim']
+        mask=seq.make_mask(seq_i,1,max_seq)
+        masked_seq=seq.make_masked_seq([seq_i],max_seq,seq_dim)        
+        masked_seq=np.array(masked_seq)
+        #masked_seq=np.squeeze(masked_seq, axis=0)
+        print("@@@@@@@@@@@@")
+        print(mask)
+        print(masked_seq.shape)
+        return self.rnn.get_distribution(masked_seq,mask)
 
     def get_seq(self,action):
         if(type(action)!=utils.actions.Action):
             raise Exception("Non action type " + str(type(action)))
-        return [self.conv(img_i)  
+        return [self.conv_net(img_i)  
                 for img_i in action.img_seq]
 
 def apply_nn(in_path,rnn,dataset_format='cp_dataset'):
     action_reader=utils.actions.read.ReadActions(dataset_format,img_seq=True)
     actions=action_reader(in_path)
-    y=[rnn(action_i) for action_i in actions]
+    test= utils.actions.raw_select(actions,0)
+    #dataset=seq.seq_dataset(in_path,True,dataset_format)
+    #print(type(dataset))
+    y=[rnn(action_i) for action_i in test]
         
 def read_rnn(rnn_path,conv_path):
     rnns=ensemble.read_ensemble(rnn_path,with_id=True)
@@ -51,8 +63,8 @@ def read_rnn(rnn_path,conv_path):
 
 if __name__ == "__main__":
     in_path="../../AArtyk/MSR/time/"
-    rnn_path="../../AArtyk/all_models/"
-    conv_path="../../AArtyk_exp/lstm/"
+    conv_path="../../AArtyk/all_models/"
+    rnn_path="../../AArtyk_exp/lstm/"
     rnn=read_rnn(rnn_path,conv_path)
     print("**********************************")
     apply_nn(in_path,rnn)
