92670080871795558eb86a9d7fbc09d464f483ed
tjacek <tjacek@student.agh.edu.pl>
2018-03-18 19:11:59 -0500

    filter outliners

--- seq/conds_dtw.py
+++ seq/conds_dtw.py
@@ -7,8 +7,14 @@ import utils.actions.read
 import utils.paths.files
 
 class DTWPairs(object):
-    def __init__(self):
-    	self.pairs={}
+    def __init__(self,pairs={},parse=None):
+        self.pairs=pairs
+        if(parse is None):
+            parse=utils.actions.read.cp_dataset
+        self.parse=parse
+    
+    def __len__(self):
+        return len(self.pairs.keys())
 
     def __setitem__(self,pair,value):
         x,y=pair
@@ -19,6 +25,25 @@ class DTWPairs(object):
     def __getitem__(self,pair):
         return self.pairs[pair[0]][pair[1]]
     
+    def __call__(self):
+        return [key_i 
+                for key_i in self.pairs.keys()
+                    if self.correct_pred(key_i)]
+
+    def correct_pred(self,correct_seq):
+        pred_seq=self.nearest_seq(correct_seq)
+        correct_cat=self.parse(correct_seq)[1]
+        pred_cat=self.parse(pred_seq)[1]
+        print(pred_cat,correct_cat)
+        return correct_cat==pred_cat
+
+    def nearest_seq(self,seq_i):
+        keys=self.get_keys(seq_i)
+        values=[self.pairs[seq_i][key_i]
+                    for key_i in keys]
+        index=np.argmin(values)
+        return keys[index]
+
     def get_keys(self,name):
         keys=self.pairs[name].keys()
         return [ key_i  for key_i in keys
@@ -42,6 +67,8 @@ def make_dtw_pairs(actions):
             print(value_ij)
     return dtw_pairs
 
-save_dtw_pairs("../../AA_dtw/max_z/seq","../../AA_dtw/max_z_pairs")
-#a=utils.paths.files.read_object("../../AA_dtw/max_z_pairs")
-#print(a.pairs.keys())
+#save_dtw_pairs("../../AA_dtw/skew/seq","../../AA_dtw/skew_pairs")
+a=utils.paths.files.read_object("../../AA_dtw/max_z_pairs")
+a=DTWPairs(a.pairs)
+print(len(a))
+print(len(a()))
