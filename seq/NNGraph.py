b37dbb7291ea9af172881b5f507091344aadc364
tjacek <tjacek@student.agh.edu.pl>
2018-03-21 21:10:03 -0500

    check clique corectly

--- seq/NNGraph.py
+++ seq/NNGraph.py
@@ -23,12 +23,11 @@ class NNGraph(object):
 
     def is_clique(self,name_i):
         nn_names=self.names[name_i]
-        nn_basic=Set(nn_names)
+        nn_basic=Set()
         for nn_i in nn_names:
-            nn_i=Set(nn_i)
-            if(nn_basic==nn_i):
-                return False
-        return True
+            nn_basic.update(self.names[nn_i])
+        print(len(nn_basic))
+        return len(nn_basic)==(len(nn_names)+1) 
         	
     def show(self):
         for name_i in self.names.keys():
@@ -36,7 +35,7 @@ class NNGraph(object):
             print(self.names[name_i])
             print(self.values[name_i])
 
-def read_nngraph(in_path,k=3):
+def read_nngraph(in_path,k=2):
     dtw_pairs=utils.paths.files.read_object(in_path)
     dtw_pairs=seq.conds_dtw.DTWPairs(dtw_pairs)
     return make_nngraph(dtw_pairs,k=k)
@@ -45,9 +44,20 @@ def make_nngraph(dtw_pairs,k=2):
     nn_graph=NNGraph()
     for name_i in dtw_pairs.names(): 
         nn_keys,nn_values=dtw_pairs.get_nn(name_i,k)
+        print(nn_keys)
         nn_graph.names[name_i]=nn_keys
         nn_graph.values[name_i]=nn_values
     return nn_graph
 
-nn_graph=read_nngraph("../../AA_dtw/eff/corl_pairs")
-print(len(nn_graph.cliqe_reduction()))
+def filter_actions(in_path,out_path,actions,dataset_format='cp_dataset'):
+    actions=Set(actions)
+    def action_helper(action_i):
+        return action_i.name in actions    
+    utils.actions.apply_select(in_path,out_path,img_seq=False,
+        selector=action_helper,dataset_format=dataset_format)
+
+nn_graph=read_nngraph("../../AA_dtw/eff/skew_pairs")
+#nn_graph.show()
+to_filter=nn_graph.cliqe_reduction()
+print(len(to_filter))
+filter_actions("../../AA_dtw/skew/seq","../../AA_dtw/eff/skew",to_filter)
