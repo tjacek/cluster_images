8ac3735d5dd09ce09dd5f911c8ab2cccb1341ac8
tjacek <tjacek@student.agh.edu.pl>
2018-03-22 19:10:33 -0500

    clique represntative

--- seq/NNGraph.py
+++ seq/NNGraph.py
@@ -18,7 +18,9 @@ class NNGraph(object):
                 if(self.is_clique(name_i)):
                     print("reduction")
                     removed_names.update(self.names[name_i])
-                used_set.append(name_i)
+                    used_set.append(self.get_clique_rep(name_i))
+                else:
+                    used_set.append(name_i)
         return used_set
 
     def is_clique(self,name_i):
@@ -28,7 +30,15 @@ class NNGraph(object):
             nn_basic.update(self.names[nn_i])
         print(len(nn_basic))
         return len(nn_basic)==(len(nn_names)+1) 
-        	
+    
+    def get_clique_rep(self,name_i):
+        nn_names=list(self.names[name_i])
+        nn_names.append(name_i)
+        dists=[ sum(self.values[nn_i]) 
+                    for nn_i in nn_names]
+        i=np.argmax(dists)
+        return nn_names[i]
+
     def show(self):
         for name_i in self.names.keys():
             print(name_i)
@@ -56,8 +66,9 @@ def filter_actions(in_path,out_path,actions,dataset_format='cp_dataset'):
     utils.actions.apply_select(in_path,out_path,img_seq=False,
         selector=action_helper,dataset_format=dataset_format)
 
-nn_graph=read_nngraph("../../AA_dtw/eff/skew_pairs")
+if __name__ == "__main__":
+    nn_graph=read_nngraph("../../AA_dtw2/skew_pairs")
 #nn_graph.show()
-to_filter=nn_graph.cliqe_reduction()
-print(len(to_filter))
-filter_actions("../../AA_dtw/skew/seq","../../AA_dtw/eff/skew",to_filter)
+    to_filter=nn_graph.cliqe_reduction()
+    print(len(to_filter))
+    filter_actions("../../AA_dtw2/skew/seq","../../AA_dtw2/eff/skew",to_filter)
