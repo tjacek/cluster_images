685e1eb3d8bc93d8351bd4f1e2d7c901028797e4
tjacek <tjacek@student.agh.edu.pl>
2018-04-17 00:21:23 -0800

    check shape in seq concate

--- seq/concat.py
+++ seq/concat.py
@@ -4,6 +4,7 @@ import numpy as np
 import utils.actions,utils.actions.read
 
 def concat_actions(in_path1,in_path2,out_path,dataset_format='cp_dataset'):
+    print(in_path1)
     action_reader=utils.actions.read.ReadActions(dataset_format,img_seq=False,as_dict=True)
     actions1=action_reader(in_path1)
     actions2=action_reader(in_path2)
@@ -17,12 +18,29 @@ def concat_actions(in_path1,in_path2,out_path,dataset_format='cp_dataset'):
 def unified_action(actionA,actionB):
     seqA=np.array(actionA.img_seq)
     seqB=np.array(actionB.img_seq)
+    print(seqA.shape)
+    print(seqB.shape)
+    seqA,seqB=check_shape(seqA,seqB)
     unified_seq=np.concatenate((seqA,seqB),axis=1)
     return utils.actions.Action(actionA.name,unified_seq,
     	                        cat=actionA.cat,person=actionA.person)
 
+def check_shape(seqA,seqB):
+    a,b=seqA.shape[0],seqB.shape[0]
+    print(a,b)
+    if(a==b):
+        return seqA,seqB
+    min_dim=min(a,b)
+    return seqA[:min_dim],seqB[:min_dim]
+
+def concat_seqs(in_path1,in_path2,out_path,n_cats=20):
+    for i in range(n_cats):
+        in1_i=str(in_path1) + '_' + str(i)
+        out_i=out_path + '_' + str(i)
+        concat_actions(in1_i,in_path2,out_i)
+
 if __name__ == "__main__":
-    in_path1='../../AA_dtw2/corl/seq'
-    in_path2="../../AA_dtw2/skew/seq" #'../../AArtyk/simple/skew/seq'
-    out_path="../../AA_dtw2/united/seq" #'../../AArtyk/simple/conc'
-    concat_actions(in_path1,in_path2,out_path)
+    in_path1='../../AA_konf/all_seqs/nn'
+    in_path2='../../AA_konf/untime/seq'
+    out_path="../../AA_konf/full_seqs/nn"
+    concat_seqs(in_path1,in_path2,out_path)
