c65a3d0193d479e39d54b091098d090159090fc4
tjacek <tjacek@student.agh.edu.pl>
2018-05-02 15:36:36 -0800

    ReduceActions class

--- utils/actions/smooth/reduce.py
+++ utils/actions/smooth/reduce.py
@@ -3,10 +3,16 @@ sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np 
 import utils.actions.smooth
 
-def make_transform(pairs,n=50):
-    y=[ pair_i[0] for pair_i in pairs]
-    X=[ pair_i[1] for pair_i in pairs]
-    svc = SVC(kernel='linear',C=1)
-    rfe = RFE(estimator=svc,n_features_to_select=n,step=1)
-    rfe.fit(X,y)
-    return rfe
+class ReduceActions(utils.actions.smooth.TimeSeriesTransform):
+    
+    def get_series_transform(self,frames):
+        y=[ pair_i[0] for pair_i in pairs]
+        X=[ pair_i[1] for pair_i in pairs]
+        svc = SVC(kernel='linear',C=1)
+        rfe = RFE(estimator=svc,n_features_to_select=n,step=1)
+        rfe.fit(X,y)
+        def redu_transform(x):
+            x=x.reshape(1, -1)
+            new_frame=rfe.transform(x)
+            return new_frame.flatten()
+        return redu_transform
