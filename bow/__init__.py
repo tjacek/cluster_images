d20ae977879b5da71e4ca36548cea3943c6aecef
tjacek <tjacek@student.agh.edu.pl>
2018-05-09 06:47:34 -0800

    separation of features using bag of words

--- bow/__init__.py
+++ bow/__init__.py
@@ -4,20 +4,45 @@ import numpy as np
 import utils.actions.read
 import utils.actions.tools
 
+def show_separation(in_path,dataset_format='cp_dataset'):
+    action_reader=utils.actions.read.ReadActions(dataset_format,img_seq=False,as_dict=False)
+    actions=action_reader(in_path)
+    by_cat=utils.actions.tools.by_category(actions)
+    n_cats=len(by_cat)
+    n_feats=utils.actions.tools.count_feats(actions)
+    histograms=[make_histograms(actions_i) for actions_i in by_cat.values()]  
+    
+    hist_by_feat=[[histograms[i][j] 
+                    for i in range(n_cats)]
+                        for j in range(n_feats)]
+    sepa_meas=[cat_separation(hist_of_feat) 
+                for hist_of_feat in hist_by_feat]
+    for sepa_i in  np.sort(sepa_meas):
+        print(sepa_i)
+    #print(len(histograms[0][0]))    
+
+
 def show_histogram(in_path,dataset_format='cp_dataset'):
     action_reader=utils.actions.read.ReadActions(dataset_format,img_seq=False,as_dict=False)
     actions=action_reader(in_path)
-    frames=utils.actions.tools.get_frames(actions)
-    features=utils.actions.tools.to_features(frames)
-    histograms=[get_histogram(feature_i) for feature_i in features]
+    histograms=make_histograms(actions)
     for histogram_i in histograms:
         print(histogram_i)
 
-def get_histogram(feature_i):
-    feature_i=np.array(feature_i)
-    n_clust=int(np.amax(feature_i))
-    if(n_clust==0):
-        return None
+def cat_separation(hist_of_feat):
+    dist_matrix=[[ np.linalg.norm(hist_i-hist_j) 
+                    for hist_i in hist_of_feat]
+                        for hist_j in hist_of_feat]
+    dist_matrix=np.array(dist_matrix)
+    return np.sum(dist_matrix)#, axis=0) 
+   
+def make_histograms(actions,n_clust=10):
+    frames=utils.actions.tools.get_frames(actions)
+    features=utils.actions.tools.to_features(frames)
+    histograms=[get_histogram(feature_i,n_clust) for feature_i in features]
+    return histograms
+
+def get_histogram(feature_i,n_clust=10):
     histogram=np.zeros( (n_clust,),dtype=float)
     for x_j in feature_i:
     	j=int(x_j)-1
@@ -27,4 +52,4 @@ def get_histogram(feature_i):
 
 if __name__ == "__main__":
     in_path="../../AA_disk3/clust_seqs/nn_0"	
-    show_histogram(in_path)
+    show_separation(in_path)
