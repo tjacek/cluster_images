06df68c5c93a62f6bdb615e791df60cc672f961b
tjacek <tjacek@student.agh.edu.pl>
2018-05-19 15:09:13 +0200

    trandform action class

--- utils/actions/tools.py
+++ utils/actions/tools.py
@@ -2,6 +2,26 @@ import numpy as np
 from sets import Set
 import utils.actions
 
+class ActionTransform(object):
+    def __init__(self,seq_transform=False,in_seq=True,out_seq=False, dataset_format='basic_dataset'):
+        self.seq_transform=seq_transform
+        self.out_seq=out_seq
+        self.read=utils.actions.read.ReadActions(dataset_format=dataset_format,img_seq=in_seq)
+        self.save=utils.actions.read.SaveActions(img_actions=out_seq)
+
+    def __call__(self,in_path,out_path,transformation):
+        actions=self.read(in_path)
+        print("Number of actions %d" % len(actions))
+        print(actions[0].img_seq[0].shape)
+        if(self.seq_transform):
+            transformed_actions=[ action_i(transformation)
+                           for action_i in actions]
+        else:
+            transformed_actions=[ action_i.transform(transformation,img_seq=self.out_seq)
+                              for action_i in actions]
+        print("OK")
+        self.save(transformed_actions,out_path)
+        
 def by_category(actions):
     n_cats=count_cats(actions)
     by_cat={ cat_i:[]  for cat_i in range(n_cats)}
