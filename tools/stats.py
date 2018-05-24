8a7dee6f262cb93ce5524329e144deb04c03e53e
tjacek <tjacek@student.agh.edu.pl>
2018-05-24 18:06:10 +0200

    check if is normal

--- tools/stats.py
+++ tools/stats.py
@@ -2,13 +2,13 @@ import sys,os
 sys.path.append(os.path.abspath('../cluster_images'))
 import numpy as np 
 import utils.actions.read
-from utils.actions.tools import count_feats
+from utils.actions.tools import count_feats,get_frames
 
 class ActionStatistic(object):
-    def __init__(self,global_stat,local_stat=None,name='stat',format='%s'):
+    def __init__(self,global_stat,local_stat=None,str_format='stats %d'):
         self.local_stat=local_stat
         self.global_stat=global_stat
-        self.str_form= name+' '+format
+        self.str_form=str_format
 
     def __call__(self,actions,as_str=False):
         if(self.local_stat is None):
@@ -26,11 +26,18 @@ def show_stats(actions):
     #                    ActionStatistic(min,len,name='min of frames'),
     #                    ActionStatistic(np.median,len,name='median of len'),
     #                    ActionStatistic(np.average,len,name='avg')]
-    basic_stats=[ ActionStatistic(len,None,name='"Number of actions'),
-                  ActionStatistic(count_feats,None,name='"Number of features'),
-                  ActionStatistic(sum,len,name='"Number of frames')]
+    basic_stats=[ ActionStatistic(len,None,str_format='Number of actions %d'),
+                  ActionStatistic(count_feats,None,str_format='Number of features %d'),
+                  ActionStatistic(sum,len,str_format='Number of frames %d'),
+                  ActionStatistic(is_normal,None,str_format='Var %d %d')]
     for stat_i in basic_stats:
         print(stat_i(actions,as_str=True))
+
+def is_normal(actions):
+    frames=get_frames(actions)
+    means=np.mean(frames,axis=0)
+    stds=np.std(frames,axis=0)
+    return np.amax(means),np.amax(stds)
 
 if __name__ == "__main__": 
     #action_path="../dataset2/exp1/cats"
