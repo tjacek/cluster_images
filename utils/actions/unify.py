132be5fb61efcfa5e7631c69c413af9b837eba18
tjacek <tjacek@student.agh.edu.pl>
2018-05-28 03:08:29 +0200

    refactore unify module

--- utils/actions/unify.py
+++ utils/actions/unify.py
@@ -7,7 +7,7 @@ import utils.actions
 import utils.actions.read
 import utils.paths
 import utils.paths.dirs
-
+import utils.actions.tools
 
 class UnifyPipeline(object):
     def __init__(self,transforms,preproc):
@@ -15,10 +15,89 @@ class UnifyPipeline(object):
         self.preproc=preproc
 
     def __call__(self,action_i):
+        action_frames=self.apply_tansform(action_i) 
+        img_seq=[ self.unify(frames_i) 
+                    for frames_i in action_frames]
+        return utils.actions.tools.new_action(action_i)
+
+    def apply_transforms(self,action_i):
         new_frames=[]
         for transform_i in self.transforms:
             new_frames+=transform_i(action_i)
-        [ preproc_i() for preproc_i in self.preproc]
+        return zip(new_frames)
+
+    def unify(self,frames):      
+        proc_frames=[self.preproc(frame_i) for frame_i in frames] 
+        return np.concatenate(proc_frames)
+
+class Rescale(object):
+    def __init__(self,new_dim=(64,64)):
+        self.new_dim=new_dim
+
+    def __call__(self,img_i):
+#        if(type(img_i)==utils.imgs.Image):
+#            img_i=img_i.get_orginal()
+        new_img=cv2.resize(img_i,self.new_dim, interpolation = cv2.INTER_CUBIC)
+        return utils.imgs.new_img(img_i,new_img)
+
+class ProjFrames(object):
+    def __init__(self,zx=True,smooth=(3,3),default_depth=150):
+        self.zx= 0 if(self.zx) else 1
+        self.default_depth=default_depth
+        self.smooth=smooth
+
+    def __call__(self,action_i):
+        action_seq,z_dim=prepare_seq(img_seq)
+        clean_img=CleanImg(action_seq.shape[self.zx],z_dim)
+        def proj_helper(img_i):
+            proj_i=clean_img()
+            for point, z in np.ndenumerate(img_i):
+                if(z!=0):
+                    i=point[self.zx]
+                    j=int(np.floor(z))
+                    proj_i[i][j]=self.default_depth
+            if(self.smooth):
+                proj_i=self.smooth_img(proj_i)      
+            return proj_i
+        return [ proj_helper(img_i) for img_i in action_i.img_seq]
+
+    def smooth_img(self,raw_img):
+        se1 = cv2.getStructuringElement(cv2.MORPH_RECT, self.smooth)
+        smooth_img = cv2.morphologyEx(raw_img, cv2.MORPH_OPEN, se1)
+    #    true_kern=np.ones(kern)
+    #    smooth_img= cv2.erode(raw_img, (3,3), iterations=1)
+    #    smooth_img=remove_isol(raw_img)
+        smooth_img=cv2.dilate(smooth_img, self.smooth, iterations=1)
+        return utils.imgs.Image(raw_img.name,smooth_img)
+
+
+class CleanImg(object):
+    def __init__(x,y):
+        self.dims=(x,y)
+
+    def __call__(self):
+        return np.zeros(self.dims)
+        
+def time_frames(self,action_i):
+    n=len(action_i)-1:
+    time_frames=[]
+    for i in range(n):
+        time_frames.append([action_i[i],action_i[i+1]])
+    return time_frames
+
+def prepare_seq(img_seq,z_dim=None,shift=1.0):
+    if(z_dim is None):
+        z_dim= float(img_seq[0].shape[0])
+    action_array=np.array(img_seq)
+    z_max=np.amax(action_array)+2.0*shift
+    z_min=np.amin( action_array[action_array!=0])
+    z_delta=z_max-z_min
+    action_array[action_array!=0]+=shift
+    action_array[action_array!=0]-=z_min
+    action_array[action_array!=0]/=z_delta
+    action_array[action_array!=0]*=z_dim
+    return action_array,int(z_dim)
+
 #def trans_img(x):    
 #    return x.T.copy()
 
@@ -72,16 +151,6 @@ class UnifyPipeline(object):
 #def unify_img(img_i,img_j):
 #    new_img=np.concatenate((img_i.get_orginal(),img_j.get_orginal()))
 #    return utils.imgs.new_img(img_i,new_img)
-
-#class Rescale(object):
-#    def __init__(self,new_dim=(60,60)):
-#        self.new_dim=new_dim
-#
-#    def __call__(self,img_i):
-#        if(type(img_i)==utils.imgs.Image):
-#            img_i=img_i.get_orginal()
-#        new_img=cv2.resize(img_i,self.new_dim, interpolation = cv2.INTER_CUBIC)
-#        return utils.imgs.new_img(img_i,new_img)#,self.new_dim)
 
 #@utils.paths.path_args
 #def proj_unify(time_path,xz_path,yz_path,out_path,
