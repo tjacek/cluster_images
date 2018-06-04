2c7ec02f33e9b02b58cf9942afb0bfe3342ed7d1
tjacek <tjacek@student.agh.edu.pl>
2018-06-04 23:56:53 +0200

    refactore frames

--- utils/actions/frames.py
+++ utils/actions/frames.py
@@ -13,67 +13,80 @@ from utils.actions.unify import Rescale
 import re
 import cv2
 
-DEFAULT_DEPTH_VALUE=150
-
-class TimeFrames(object):
-    def __init__(self, new_dim=None):
+class Rescale(object):
+    def __init__(self,new_dim=(64,64)):
         self.new_dim=new_dim
-        self.rescale=Rescale(new_dim)
 
-    def __call__(self,img_seq):
-        print(type(img_seq[0]))
-        n=len(img_seq)-1
-        def unify_helper(img_i,img_j):
-            img_i=img_i.get_orginal()
-            img_j=img_j.get_orginal()
-            if(self.new_dim!=None):
-                img_i=self.rescale(img_i)
-                img_j=self.rescale(img_j)
-            united_img=np.array([img_i, img_j])
-            new_x=united_img.shape[0]*united_img.shape[1]
-            new_y=united_img.shape[2]
-            img2D=united_img.reshape((new_x,new_y))
-            return utils.imgs.Image(img_i.name,img2D)
-        print(len(img_seq))
-        new_seq=[ unify_helper(img_seq[i], img_seq[i+1])
-                 for i in range(n)]
-        print(len(new_seq))
-        return new_seq
-
-class MotionFrames(object):
-    def __init__(self,tau=5,scale=20,diff=False):
-        self.tau=int(tau)
-        self.scale=scale
-        self.diff=diff
-
-    def __call__(self,img_seq):
-        if(self.diff):
-            diff_seq=diff_frames(img_seq)
+    def __call__(self,imgs):
+        if(type(imgs)==list):
+            return [ self.rescale_img(img_i) for img_i in imgs]
         else:
-            diff_seq=img_seq
-        n=len(diff_seq)
-        def motion_helper(i):
-            motion_img=np.zeros(diff_seq[0].shape)
-            for t in range(self.tau):
-                index=i-t
-                if(index >= 0):
-                    diff_t=diff_seq[index]
-                    motion_img[diff_t!=0]=t
-            name_i=diff_seq[i].name
-            return utils.imgs.Image(name_i,motion_img)
-        return [ self.scale*motion_helper(i)
-                   for i in range(n) ]
+            return self.rescale_img(imgs)
 
-def diff_frames(img_seq,threshold=0.1):
-    n=len(img_seq)-1
-    def diff_helper(i):
-        diff_img=np.abs(img_seq[i]-img_seq[i+1])
-        #print(diff_img.is_normal())
-        diff_img[ diff_img>=threshold]=DEFAULT_DEPTH_VALUE
-        diff_img[ diff_img<threshold]=0.0
-        return diff_img
-    return [  diff_helper(i)
-              for i in range(n)]
+    def rescale_img(self,img_i):
+        new_img=cv2.resize(img_i,self.new_dim, interpolation = cv2.INTER_CUBIC)
+        return new_img#utils.imgs.new_img(img_i,new_img)
+
+class ProjFrames(object):
+    def __init__(self,zx=True,smooth=(10,10),default_depth=150.0):
+        self.zx= 0 if(zx) else 1
+        self.default_depth=default_depth
+        self.smooth=smooth
+
+    def __call__(self,action_i):
+        print(action_i.name)
+        action_seq,z_dim=prepare_seq(action_i.img_seq)
+        dim_0=action_seq.shape[self.zx+1]
+        dim_1=z_dim+2
+        clean_img=CleanImg(dim_0,dim_1)
+        def proj_helper(img_i):
+            proj_i=clean_img()
+            for point, z in np.ndenumerate(img_i):
+                if(z!=0):
+                    i=point[self.zx]
+                    j=int(np.floor(z))
+                    proj_i[i][j]=self.default_depth
+            if(self.smooth):
+                proj_i=self.smooth_img(proj_i)      
+            return proj_i
+        return [ proj_helper(img_i) for img_i in action_i.img_seq]
+
+    def smooth_img(self,raw_img):
+    #    se1 = cv2.getStructuringElement(cv2.MORPH_RECT, self.smooth)
+    #    smooth_img = cv2.morphologyEx(raw_img, cv2.MORPH_OPEN, se1)
+        true_kern=np.ones(self.smooth)
+    #    smooth_img= cv2.erode(raw_img, (3,3), iterations=1)
+    #    smooth_img=remove_isol(raw_img)
+        smooth_img=cv2.dilate(raw_img, true_kern, iterations=1)
+        return smooth_img
+
+class CleanImg(object):
+    def __init__(self,x,y):
+        self.dims=(x,y)
+
+    def __call__(self):
+        return np.zeros(self.dims)
+        
+def time_frames(action_i):
+    n=len(action_i)-1
+    time_frames=[]
+    for i in range(n):
+        time_frames.append([action_i[i],action_i[i+1]])
+    return time_frames
+
+def prepare_seq(img_seq,z_dim=None,shift=1.0):
+    if(z_dim is None):
+        z_dim= float(img_seq[0].shape[0])
+    action_array=np.array(img_seq)
+    z_max=np.amax(action_array)+2.0*shift
+    z_min=np.amin( action_array[action_array!=0])
+    z_delta=z_max-z_min
+    action_array[action_array!=0]+=shift
+    action_array[action_array!=0]-=z_min
+    action_array[action_array!=0]/=z_delta
+    action_array[action_array!=0]*=z_dim
+    return action_array,int(z_dim)
+
 
 class BoundFrames(object):
     def __init__(self,all_frames=True,clean=None,smooth_img=False):
@@ -112,82 +125,11 @@ class BoundFrames(object):
         extract_box=utils.actions.bound.ExtractBox(points)
         return extract_box
 
-class SmoothImg(object):
-    def __init__(self, kern=(3,3)):
-        self.kern = kern
-    
-    def __call__(self,raw_img):
-        if(self.kern!=None):
-            smooth_img=cv2.blur(raw_img,self.kern)
-            smooth_img=smooth_img.astype(np.uint8)
-        else:
-            smooth_img=raw_img.astype(np.uint8)
-        ret,binary_img=cv2.threshold(smooth_img,5,255,cv2.THRESH_BINARY)
-        binary_img[binary_img!=0]=DEFAULT_DEPTH_VALUE
-        return utils.imgs.Image(raw_img.name,binary_img)
-
 def bound_local(img_seq):
     n=len(img_seq)-1
     new_seq=[utils.actions.bound.nonzero_double(img_seq[i],img_seq[i+1],True) 
                    for i in range(n)]     
     return new_seq
-
-
-class ProjFrames(object):
-    def __init__(self,zx=True,smooth=False,scale_z=True):
-        self.zx=zx
-        self.smooth=smooth
-        self.scale_z=scale_z
-
-    def __call__(self,img_seq):
-        action_seq,z_dim=prepare_seq(img_seq)
-        if(self.zx):
-            clean_helper= lambda : np.zeros( (img_i.shape[0],z_dim))
-        else:
-            clean_helper= lambda : np.zeros( (img_i.shape[1],z_dim))
-        def proj_helper(k,img_k):
-            proj_xz=clean_helper()
-            for (x, y), z in np.ndenumerate(img_i):
-                if(z!=0):
-                    i,j=self.get_index(x,y,z)
-                    proj_xz[i][j]=DEFAULT_DEPTH_VALUE
-            name_k=img_seq[k].name
-            print(name_k)
-            new_img=utils.imgs.Image(name_k,proj_xz)
-            if(self.smooth):
-                new_img=self.smooth_img(new_img)
-            return new_img
-        return [proj_helper( i,img_i) 
-                   for i,img_i in enumerate(action_seq)]
-
-    def get_index(self,x,y,z):
-        z=np.floor(z)
-        if(self.zx):
-            return int(x),int(z)
-        else:
-            return int(y),int(z)
-
-    def smooth_img(self,raw_img,kern=(3,3)):
-        se1 = cv2.getStructuringElement(cv2.MORPH_RECT, (2,2))
-        smooth_img = cv2.morphologyEx(raw_img, cv2.MORPH_OPEN, se1)
-    #    true_kern=np.ones(kern)
-    #    smooth_img= cv2.erode(raw_img, (3,3), iterations=1)
-    #    smooth_img=remove_isol(raw_img)
-        smooth_img=cv2.dilate(smooth_img, (4,4), iterations=1)
-        return utils.imgs.Image(raw_img.name,smooth_img)
-
-def prepare_seq(img_seq,z_dim=None,shift=1.0):
-    if(z_dim is None):
-        z_dim= float(img_seq[0].shape[0])
-    action_array=np.array(img_seq)
-    z_max=np.amax(action_array)+2.0*shift
-    z_min=np.amin( action_array[action_array!=0])
-    z_delta=z_max-z_min
-    action_array[action_array!=0]+=shift
-    action_array[action_array!=0]-=z_min
-    action_array[action_array!=0]/=z_delta
-    action_array[action_array!=0]*=z_dim
-    return action_array,int(z_dim)
 
 def remove_isol(img_i):
     kernel = np.ones((3,3),np.float32)
